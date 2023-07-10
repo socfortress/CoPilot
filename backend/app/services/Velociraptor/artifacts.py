981c11996a4c3515ef15b23120133ffec7024c75
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2023-07-10 16:44:43 -0500

    Create artifacts.py

---
+++ backend/app/services/Velociraptor/artifacts.py
@@ -0,0 +1,121 @@
+from typing import Dict
+from loguru import logger
+from pyvelociraptor import api_pb2
+from werkzeug.utils import secure_filename
+from app.services.Velociraptor.universal import UniversalService
+import json
+
+
+class ArtifactsService:
+    """
+    A service class that encapsulates the logic for pulling artifacts from Velociraptor.
+    """
+
+    def __init__(self):
+        self.universal_service = UniversalService()
+
+    def _create_query(self, query: str):
+        """
+        Create a query string.
+
+        Args:
+            query (str): The query to be executed.
+
+        Returns:
+            str: The created query string.
+        """
+        return query
+
+    def _get_artifact_key(self, client_id: str, artifact: str):
+        """
+        Construct the artifact key.
+
+        Args:
+            client_id (str): The ID of the client.
+            artifact (str): The name of the artifact.
+
+        Returns:
+            str: The constructed artifact key.
+        """
+        return f"collect_client(client_id='{client_id}', artifacts=['{artifact}'])"
+
+    def collect_artifacts(self):
+        """
+        Collect the artifacts from Velociraptor.
+
+        Returns:
+            dict: A dictionary with the success status, a message, and potentially the artifacts.
+        """
+        query = self._create_query("SELECT name FROM artifact_definitions()")
+        return self.universal_service.execute_query(query)
+
+    def collect_artifacts_prefixed(self, prefix: str):
+        """
+        Collect the artifacts from Velociraptor that have a name beginning with a specific prefix.
+
+        Args:
+            prefix (str): The prefix to filter the artifacts.
+
+        Returns:
+            dict: A dictionary with the success status, a message, and potentially the artifacts.
+        """
+        artifacts_response = self.collect_artifacts()
+        if not artifacts_response["success"]:
+            return artifacts_response
+
+        filtered_artifacts = [
+            artifact
+            for artifact in artifacts_response["results"]
+            if artifact["name"].startswith(prefix)
+        ]
+
+        return {
+            "success": True,
+            "message": f"Successfully collected {prefix} artifacts",
+            "artifacts": filtered_artifacts,
+        }
+
+    def collect_artifacts_linux(self):
+        return self.collect_artifacts_prefixed("Linux.")
+
+    def collect_artifacts_windows(self):
+        return self.collect_artifacts_prefixed("Windows.")
+    
+    def collect_artifacts_macos(self):
+        return self.collect_artifacts_prefixed("MacOS.")
+
+    def run_artifact_collection(self, client_id: str, artifact: str):
+        """
+        Run an artifact collection on a specific client.
+
+        Args:
+            client_id (str): The ID of the client.
+            artifact (str): The name of the artifact.
+
+        Returns:
+            dict: A dictionary with the success status, a message, and potentially the results.
+        """
+        try:
+            query = self._create_query(
+                f"SELECT collect_client(client_id='{client_id}', artifacts=['{artifact}']) FROM scope()"
+            )
+            flow = self.universal_service.execute_query(query)
+            logger.info(f"Successfully ran artifact collection on {flow}")
+
+            artifact_key = self._get_artifact_key(client_id, artifact)
+            flow_id = flow["results"][0][artifact_key]["flow_id"]
+            logger.info(f"Successfully ran artifact collection on {flow_id}")
+
+            completed = self.universal_service.watch_flow_completion(flow_id)
+            logger.info(f"Successfully watched flow completion on {completed}")
+
+            results = self.universal_service.read_collection_results(
+                client_id, flow_id, artifact
+            )
+            return results
+        except Exception as err:
+            logger.error(f"Failed to run artifact collection: {err}")
+            return {
+                "message": "Failed to run artifact collection",
+                "success": False,
+            }
