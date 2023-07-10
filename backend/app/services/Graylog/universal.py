747052cf0a2cf4a64897f78363d791300c41f9f3
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2023-07-10 16:43:57 -0500

    Create universal.py

---
+++ backend/app/services/Graylog/universal.py
@@ -0,0 +1,41 @@
+from app.models.agents import (
+    AgentMetadata,
+    agent_metadata_schema,
+    agent_metadatas_schema,
+)
+from app import db
+from datetime import datetime
+import requests
+from loguru import logger
+from app.models.connectors import connector_factory, Connector, GraylogConnector
+
+
+class UniversalService:
+    """
+    A service class that encapsulates the logic for polling messages from Graylog.
+    """
+
+    def __init__(self) -> None:
+        self.collect_graylog_details("Graylog")
+
+    def collect_graylog_details(self, connector_name: str):
+        """
+        Collects the details of the Graylog connector.
+
+        Args:
+            connector_name (str): The name of the Graylog connector.
+
+        Returns:
+            tuple: A tuple containing the connection URL, username, and password.
+        """
+        connector_instance = connector_factory.create(connector_name, connector_name)
+        connection_successful = connector_instance.verify_connection()
+        if connection_successful:
+            connection_details = Connector.get_connector_info_from_db(connector_name)
+            return (
+                connection_details.get("connector_url"),
+                connection_details.get("connector_username"),
+                connection_details.get("connector_password"),
+            )
+        else:
+            return None, None, None
