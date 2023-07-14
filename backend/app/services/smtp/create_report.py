f86bad51f8e15ecb72774013d3ded43b86377a4b
Taylor <taylor.walton@socfortress.co>
2023-07-14 11:52:15 -0500

    refactor for modular and ease of reading

--- backend/app/services/smtp/create_report.py
+++ backend/app/services/smtp/create_report.py
@@ -1,6 +1,7 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
+
 def create_pdf():
     c = canvas.Canvas("report.pdf", pagesize=letter)
     width, height = letter
--- backend/app/services/smtp/send_report.py
+++ backend/app/services/smtp/send_report.py
@@ -1,44 +1,56 @@
 import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-from app.services.smtp.universal import UniversalEmailCredentials, EmailTemplate  # add EmailTemplate to the import
+
 from app.services.smtp.create_report import create_pdf
+from app.services.smtp.universal import EmailTemplate
+from app.services.smtp.universal import UniversalEmailCredentials
+
+
+def create_email_message(subject: str, from_email: str, to_email: str, body: str) -> MIMEMultipart:
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "html"))
+    return msg
+
+
+def attach_pdf(msg: MIMEMultipart, filename: str) -> MIMEMultipart:
+    with open(filename, "rb") as attachment_file:
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment_file.read())
+        encoders.encode_base64(part)
+        part.add_header("Content-Disposition", f"attachment; filename= {filename}")
+        msg.attach(part)
+    return msg
+
 
 def send_email_with_pdf():
     # Generate the PDF report
     create_pdf()
 
+    # Get email credentials
     try:
-        credentials = UniversalEmailCredentials.read_all()['emails_configured'][0]
+        credentials = UniversalEmailCredentials.read_all()["emails_configured"][0]
     except IndexError:
         raise Exception("No email credentials found in the database.")
 
-    msg = MIMEMultipart()
-    msg['From'] = credentials['email']
-    msg['To'] = 'walton.taylor23@gmail.com'
-    msg['Subject'] = 'Test Report'
+    # Render the email body
+    template = EmailTemplate("email_template")
+    body = template.render_html_body(template_name="email_template")
 
-    # Use EmailTemplate for the email body
-    template = EmailTemplate('email_template')  # replace 'email_template' with the name of your template without .jinja
-    body = template.render_html_body(template_name="email_template")  # replace this line
-    msg.attach(MIMEText(body, 'html'))  # add 'html' as the second argument to MIMEText
+    # Create the email message and attach the PDF
+    msg = create_email_message("Test Report", credentials["email"], "walton.taylor23@gmail.com", body)
+    msg = attach_pdf(msg, "report.pdf")
 
-    filename = 'report.pdf'
-    attachment = open(filename, 'rb')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename= {}".format(filename))
-    msg.attach(part)
-
-    server = smtplib.SMTP(credentials['smtp_server'], credentials['smtp_port'])
-    server.starttls()
-    server.login(credentials['email'], credentials['password'])
-    text = msg.as_string()
-    server.sendmail(credentials['email'], 'walton.taylor23@gmail.com', text)
-    server.quit()
+    # Send the email
+    with smtplib.SMTP(credentials["smtp_server"], credentials["smtp_port"]) as server:
+        server.starttls()
+        server.login(credentials["email"], credentials["password"])
+        text = msg.as_string()
+        server.sendmail(credentials["email"], "walton.taylor23@gmail.com", text)
 
     return {"message": "Report sent successfully", "success": True}
-
