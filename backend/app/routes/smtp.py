a4fe9d38a899f19af9f02468a6e0ab57a5c5881f
Taylor <taylor.walton@socfortress.co>
2023-07-14 10:42:05 -0500

    only allow one email to be configured to send reports

--- backend/app/routes/smtp.py
+++ backend/app/routes/smtp.py
@@ -1,6 +1,3 @@
-from typing import Any
-from typing import Dict
-
 from flask import Blueprint
 from flask import jsonify
 from flask import request
@@ -22,10 +19,10 @@ def put_credentials() -> jsonify:
     if not request.is_json:
         return jsonify({"message": "Missing JSON in request", "success": False}), 400
 
-    email = request.json.get('email', None)
-    password = request.json.get('password', None)
-    smtp_server = request.json.get('smtp_server', None)
-    smtp_port = request.json.get('smtp_port', None)
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    smtp_server = request.json.get("smtp_server", None)
+    smtp_port = request.json.get("smtp_port", None)
 
     if not all([email, password, smtp_server, smtp_port]):
         return jsonify({"message": "Missing JSON in request", "success": False}), 400
@@ -33,6 +30,7 @@ def put_credentials() -> jsonify:
     new_credential = UniversalEmailCredentials.create(email, password, smtp_server, smtp_port)
 
     return jsonify(new_credential), 201
+
 
 @bp.route("/smtp/credentials", methods=["GET"])
 def get_credentials() -> jsonify:
--- backend/app/services/smtp/universal.py
+++ backend/app/services/smtp/universal.py
@@ -1,13 +1,18 @@
-from typing import Dict, Optional, Union, List
-from loguru import logger
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
+
 from app import db
-from app.models.smtp import EmailCredentials, email_credentials_schema
+from app.models.smtp import EmailCredentials
+from app.models.smtp import email_credentials_schema
+
 
 class UniversalEmailCredentials:
     @staticmethod
     def create(email: str, password: str, smtp_server: str, smtp_port: int) -> Dict[str, Union[int, str]]:
         """
-        Create new email credentials and add them to the database.
+        Delete all existing email credentials, create new email credentials and add them to the database.
 
         :param email: Email address.
         :param password: Email password.
@@ -15,10 +20,16 @@ class UniversalEmailCredentials:
         :param smtp_port: SMTP server port.
         :return: Dictionary of the created email credentials.
         """
+        UniversalEmailCredentials.delete_all()
+
         new_credential = EmailCredentials(email, password, smtp_server, smtp_port)
         db.session.add(new_credential)
         db.session.commit()
-        return {"message": "Email credentials created successfully.", "success": True, "email_settings": email_credentials_schema.dump(new_credential)}
+        return {
+            "message": "Email credentials created successfully.",
+            "success": True,
+            "email_settings": email_credentials_schema.dump(new_credential),
+        }
 
     @staticmethod
     def read_by_id(id: int) -> Optional[Dict[str, Union[int, str]]]:
@@ -50,13 +61,22 @@ class UniversalEmailCredentials:
         :return: List of dictionaries of all email credentials.
         """
         credentials = EmailCredentials.query.all()
-        return {"message": "Email credentials retrieved successfully.", "success": True, "emails_configured": email_credentials_schema.dump(credentials, many=True)}
+        return {
+            "message": "Email credentials retrieved successfully.",
+            "success": True,
+            "emails_configured": email_credentials_schema.dump(credentials, many=True),
+        }
 
     @staticmethod
-    def update(id: int, email: Optional[str] = None, password: Optional[str] = None,
-               smtp_server: Optional[str] = None, smtp_port: Optional[int] = None) -> Optional[Dict[str, Union[int, str]]]:
+    def update(
+        id: int,
+        email: Optional[str] = None,
+        password: Optional[str] = None,
+        smtp_server: Optional[str] = None,
+        smtp_port: Optional[int] = None,
+    ) -> Optional[Dict[str, Union[int, str]]]:
         """
-        Update existing email credentials.
+        Update existing email credentials. If more than one credential exists, delete all and create a new one.
 
         :param id: ID of the email credentials.
         :param email: New email address.
@@ -76,20 +96,26 @@ class UniversalEmailCredentials:
             credential.smtp_server = smtp_server
         if smtp_port is not None:
             credential.smtp_port = smtp_port
+
+        UniversalEmailCredentials.delete_all_except_id(id)
+
         db.session.commit()
         return email_credentials_schema.dump(credential)
 
     @staticmethod
-    def delete(id: int) -> Optional[Dict[str, str]]:
+    def delete_all():
         """
-        Delete email credentials by their id.
-
-        :param id: ID of the email credentials.
-        :return: Message of the deletion operation or None if not found.
+        Delete all email credentials from the table.
         """
-        credential = EmailCredentials.query.get(id)
-        if not credential:
-            return None
-        db.session.delete(credential)
+        EmailCredentials.query.delete()
         db.session.commit()
-        return {'message': f'Email credential with ID {id} deleted.'}
+
+    @staticmethod
+    def delete_all_except_id(id: int):
+        """
+        Delete all email credentials except the one with the given id.
+
+        :param id: ID of the email credentials to keep.
+        """
+        EmailCredentials.query.filter(EmailCredentials.id != id).delete()
+        db.session.commit()
