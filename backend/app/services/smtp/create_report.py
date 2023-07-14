b9fc29f7a85b5e496d4b35b49f3a6958f41fbc69
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2023-07-14 14:42:41 -0600

    combine charts to one pdf (#27)

--- backend/app/services/smtp/create_report.py
+++ backend/app/services/smtp/create_report.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import matplotlib
 from loguru import logger
 from reportlab.lib.pagesizes import letter
@@ -43,26 +45,25 @@ def create_pie_chart(alerts: dict, title: str, output_filename: str) -> None:
     plt.savefig(output_filename)
 
 
-def create_pdf(title: str, image_filename: str, pdf_filename: str) -> None:
+def create_pdf(title: str, image_filenames: List[str], pdf_filename: str) -> None:
     c = canvas.Canvas(pdf_filename, pagesize=letter)
     width, height = letter
     c.setFont("Helvetica", 24)
     c.drawString(30, height - 50, title)
-    c.drawImage(image_filename, 50, height / 2, width=400, height=300)
+
+    image_height = height / 2
+    for i, image_filename in enumerate(image_filenames):
+        c.drawImage(image_filename, 50, image_height - i * 300, width=400, height=300)
     c.save()
 
 
-def create_alerts_by_host_pdf() -> None:
+def create_alerts_report_pdf() -> None:
     service = AlertsService()
+
     alerts_by_host = fetch_alert_data(service, service.collect_alerts_by_host)
-
     create_bar_chart(alerts_by_host, "Number of Alerts by Host", "alerts_by_host.png")
-    create_pdf("Test", "alerts_by_host.png", "alerts_by_host_report.pdf")
 
-
-def create_alerts_by_rules_pdf() -> None:
-    service = AlertsService()
     alerts_by_rules = fetch_alert_data(service, service.collect_alerts_by_rule)
-
     create_pie_chart(alerts_by_rules, "Number of Alerts by Rule", "alerts_by_rule.png")
-    create_pdf("Test", "alerts_by_rule.png", "alerts_by_rule_report.pdf")
+
+    create_pdf("Test", ["alerts_by_host.png", "alerts_by_rule.png"], "alerts_report.pdf")
