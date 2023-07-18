e3686f76ed1b3deea1e59028943984b519d60eff
Taylor <taylor.walton@socfortress.co>
2023-07-18 11:11:55 -0500

    search ioc value exists within the provided field name for wazuh index

--- backend/app/routes/threatintel.py
+++ backend/app/routes/threatintel.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 from loguru import logger
 
 from app.services.threat_intel.socfortress.universal import (
     SocfortressThreatIntelService,
 )
+from app.services.WazuhIndexer.ioc_search import IocSearchService
 
 bp = Blueprint("threatintel", __name__)
 
@@ -20,3 +22,19 @@ def get_socfortress_threatintel(ioc_value: str) -> jsonify:
     logger.info("Received request to check IoC in Socfortress Threat Intel")
     ioc_enriched = SocfortressThreatIntelService("SocfortressThreatIntel").invoke_socfortress_threat_intel(data=ioc_value)
     return jsonify(ioc_enriched)
+
+
+@bp.route("/threatintel/search/wazuh", methods=["POST"])
+def search_wazuh_threatintel() -> jsonify:
+    """
+    Endpoint to search IoC in Wazuh Threat Intel.
+
+    Returns:
+        jsonify: A JSON response containing the list of all alerts from Wazuh.
+    """
+    logger.info("Received request to search IoC in Wazuh Threat Intel")
+    service = IocSearchService()
+    field_name = request.json.get("field_name")
+    time_range = request.json.get("time_range")
+    ioc_searched = service.search_ioc(field_name=field_name, time_range=time_range)
+    return jsonify(ioc_searched)
