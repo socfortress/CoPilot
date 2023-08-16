6081ec3f5dcd1af6384d4fba947bc35f9ebcc04a
Davide Di Modica <webmaster.ddm@gmail.com>
2023-08-16 22:10:43 +0200

    added indices types

---
+++ src/types/indices.d.ts
@@ -0,0 +1,55 @@
+export interface Index {
+    docs_count: string
+    health: IndexHealth
+    index: string
+    replica_count: string
+    store_size: string
+}
+
+// TODO: Better to use a status instead of a color
+export enum IndexHealth {
+    GREEN = "green",
+    YELLOW = "yellow",
+    RED = "red"
+}
+
+export interface IndexAllocation {
+    disk_available: null | string
+    disk_percent: null | string
+    disk_total: null | string
+    disk_used: null | string
+    node: string | "UNASSIGNED"
+}
+
+export interface IndexShard {
+    index: string
+    node: null | string
+    shard: string
+    size: null | string
+    state: IndexShardState
+}
+
+export enum IndexShardState {
+    STARTED = "STARTED",
+    UNASSIGNED = "UNASSIGNED"
+}
+
+export interface ClusterHealth {
+    active_primary_shards: number
+    active_shards: number
+    active_shards_percent_as_number: number
+    cluster_name: string
+    delayed_unassigned_shards: number
+    discovered_cluster_manager: boolean
+    discovered_master: boolean
+    initializing_shards: number
+    number_of_data_nodes: number
+    number_of_in_flight_fetch: number
+    number_of_nodes: number
+    number_of_pending_tasks: number
+    relocating_shards: number
+    status: string
+    task_max_waiting_in_queue_millis: number
+    timed_out: boolean
+    unassigned_shards: number
+}
