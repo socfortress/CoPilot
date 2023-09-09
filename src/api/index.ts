2d881e89d3c42eeae3575c8891acdcd78c770e41
Taylor <taylor.walton@socfortress.co>
2023-09-09 16:27:11 -0500

    add graylog to api index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import connectors from "./connectors"
 import indices from "./indices"
 import agents from "./agents"
+import graylog from "./graylog"
 
 export default {
     connectors,
     indices,
-    agents
+    agents,
+    graylog
 }
