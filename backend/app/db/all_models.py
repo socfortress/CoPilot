4257bba47cea3a4aaba6ec43fe330aa6f306bc8a
Taylor <taylor.walton@socfortress.co>
2023-10-05 08:29:29 -0500

    uncomment

--- backend/app/db/all_models.py
+++ backend/app/db/all_models.py
@@ -1,6 +1,6 @@
 # all_models.py
-# from app.auth.models.users import User
-# from app.connectors.models import Connectors
-# from app.connectors.sublime.models.alerts import SublimeAlerts
-# from app.connectors.wazuh_manager.models.rules import DisabledRule
-# from app.db.universal_models import Customers, CustomersMeta, Agents
+from app.auth.models.users import User
+from app.connectors.models import Connectors
+from app.connectors.sublime.models.alerts import SublimeAlerts
+from app.connectors.wazuh_manager.models.rules import DisabledRule
+from app.db.universal_models import Customers, CustomersMeta, Agents
