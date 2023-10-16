86fc5150b25df104f3c0859747c1fb5bc9ce1d2c
Davide Di Modica <webmaster.ddm@gmail.com>
2023-10-16 17:59:33 +0200

    rm backend folder

--- backend/app/db/db_session.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Session
-from sqlmodel import create_engine
-
-from settings import SQLALCHEMY_DATABASE_URI
-
-engine = create_engine(SQLALCHEMY_DATABASE_URI)
-session = Session(bind=engine)
