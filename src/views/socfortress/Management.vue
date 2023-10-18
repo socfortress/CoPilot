91927d9bcfde3c9be0a4ced76abed8abe92dbc5f
Davide Di Modica <webmaster.ddm@gmail.com>
2023-10-18 18:29:02 +0200

    added management page

---
+++ src/views/socfortress/Management.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="page">
+		<n-tabs type="line" animated>
+			<n-tab-pane name="messages" tab="Messages">
+				<Messages />
+			</n-tab-pane>
+			<n-tab-pane name="alerts" tab="Alerts">alerts</n-tab-pane>
+			<n-tab-pane name="events" tab="Events">events</n-tab-pane>
+			<n-tab-pane name="streams" tab="Streams">streams</n-tab-pane>
+			<n-tab-pane name="inputs" tab="Inputs">inputs</n-tab-pane>
+		</n-tabs>
+	</div>
+</template>
+
+<script setup>
+import { NTabs, NTabPane } from "naive-ui"
+import Messages from "@/components/graylog/Messages/List.vue"
+</script>
+
+<style lang="scss" scoped></style>
