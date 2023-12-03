34a82f08a282ed63fb02b05abb23d1825d701179
Davide Di Modica <webmaster.ddm@gmail.com>
2023-12-03 11:50:40 +0100

    updated soc users page

--- src/components/soc/SocUsersList.vue
+++ src/components/soc/SocUsersList.vue
@@ -1,41 +1,64 @@
 <template>
 	<div class="soc-users-list">
-		<n-data-table :columns="columns" :data="usersList" :bordered="false" :loading="loading" />
+		<n-spin :show="loading">
+			<n-scrollbar x-scrollable style="width: 100%">
+				<n-table :bordered="false" class="min-w-max">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Login</th>
+							<th>Name</th>
+							<th>Active</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="user of usersList" :key="user.user_id">
+							<td>
+								<div class="flex gap-3 items-center">
+									<span>#{{ user.user_id }}</span>
+									<n-tooltip trigger="hover">
+										<template #trigger>
+											<Icon :name="InfoIcon" :size="16" class="cursor-help"></Icon>
+										</template>
+										{{ user.user_uuid }}
+									</n-tooltip>
+								</div>
+							</td>
+							<td>
+								{{ user.user_login }}
+							</td>
+							<td>
+								{{ user.user_name }}
+							</td>
+							<td>
+								<strong
+									class="active-field"
+									:class="{ success: user.user_active, warning: !user.user_active }"
+								>
+									{{ user.user_active ? "Yes" : "No" }}
+								</strong>
+							</td>
+						</tr>
+					</tbody>
+				</n-table>
+			</n-scrollbar>
+		</n-spin>
 	</div>
 </template>
 
 <script setup lang="ts">
+// TODO: add user row highlight by query params
 import { ref, onBeforeMount } from "vue"
-import { useMessage, NDataTable } from "naive-ui"
+import { useMessage, NTable, NTooltip, NScrollbar, NSpin } from "naive-ui"
+import Icon from "@/components/common/Icon.vue"
 import Api from "@/api"
 import type { SocUser } from "@/types/soc/user.d"
+
+const InfoIcon = "carbon:information"
 
 const message = useMessage()
 const loading = ref(false)
 const usersList = ref<SocUser[]>([])
-
-const columns = [
-	{
-		title: "ID",
-		key: "user_id"
-	},
-	{
-		title: "Login",
-		key: "user_login"
-	},
-	{
-		title: "Name",
-		key: "user_name"
-	},
-	{
-		title: "UUID",
-		key: "user_uuid"
-	},
-	{
-		title: "Active",
-		key: "user_active"
-	}
-]
 
 function getData() {
 	loading.value = true
@@ -63,3 +86,18 @@ onBeforeMount(() => {
 	getData()
 })
 </script>
+
+<style lang="scss" scoped>
+.soc-users-list {
+	border-radius: var(--border-radius);
+	overflow: hidden;
+	.active-field {
+		&.success {
+			color: var(--success-color);
+		}
+		&.warning {
+			color: var(--warning-color);
+		}
+	}
+}
+</style>
