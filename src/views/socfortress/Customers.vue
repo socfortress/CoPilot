231d422d7e4cf55797519dc92379eca1f5db077e
Davide Di Modica <webmaster.ddm@gmail.com>
2023-12-14 10:00:12 +0100

    added customers page

---
+++ src/views/socfortress/Customers.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="page">...Customers</div>
+</template>
+
+<script setup>
+// import AlertsList from "@/components/alerts/AlertsList.vue"
+</script>
+
+<style lang="scss" scoped></style>
