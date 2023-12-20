8603ced595a61edac8b946ac02cbb27f88d81301
Taylor <taylor.walton@socfortress.co>
2023-12-20 13:58:12 -0600

    post route to add new event config

--- backend/app/integrations/alert_creation_settings/routes/alert_creation_settings.py
+++ backend/app/integrations/alert_creation_settings/routes/alert_creation_settings.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import joinedload
 from typing import List
 
 from app.db.db_session import get_session
-from app.integrations.alert_creation_settings.schema.alert_creation_settings import AlertCreationSettingsCreate, AlertCreationSettingsResponse, EventOrderCreate
+from app.integrations.alert_creation_settings.schema.alert_creation_settings import AlertCreationSettingsCreate, AlertCreationSettingsResponse, EventOrderCreate, EventOrderResponse
 from app.integrations.alert_creation_settings.models.alert_creation_settings import AlertCreationSettings, EventOrder, AlertCreationEventConfig
 
 alert_creation_settings_router = APIRouter()
@@ -70,6 +70,45 @@ async def get_alert_creation_settings(
         raise HTTPException(status_code=404, detail="Alert creation settings not found.")
 
     return settings
+
+@alert_creation_settings_router.post(
+    "/{customer_name}/event",
+    response_model=EventOrderResponse,
+    description="Add a new event to a customer's alert creation settings.",
+)
+async def add_event_order(
+    customer_name: str,
+    event_order: EventOrderCreate,
+    session: AsyncSession = Depends(get_session),
+):
+    result = await session.execute(
+        select(AlertCreationSettings)
+        .options(joinedload(AlertCreationSettings.event_orders).joinedload(EventOrder.event_configs))
+        .where(AlertCreationSettings.customer_name == customer_name)
+    )
+    settings = result.scalars().first()
+
+    if not settings:
+        raise HTTPException(status_code=404, detail="Alert creation settings not found.")
+
+    # Create new event order and configs
+    event_order_db = EventOrder(order_label=event_order.order_label, alert_creation_settings=settings)
+    session.add(event_order_db)
+    for event_config in event_order.event_configs:
+        event_config_db = AlertCreationEventConfig(**event_config.dict(), event_order=event_order_db)
+        session.add(event_config_db)
+
+    await session.commit()
+
+    # Query the EventOrder instance again to ensure event_configs are loaded
+    result = await session.execute(
+        select(EventOrder)
+        .options(joinedload(EventOrder.event_configs))
+        .where(EventOrder.id == event_order_db.id)
+    )
+    event_order_db = result.scalars().first()
+
+    return event_order_db
 
 
 @alert_creation_settings_router.put(
