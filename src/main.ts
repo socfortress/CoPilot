ccfbf1b42b3cc1484eba7de88c9915c6865ff1d8
Davide Di Modica <webmaster.ddm@gmail.com>
2023-12-29 14:03:29 +0100

    updated dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import { createI18n } from "vue-i18n"
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
-import VueApexCharts from "vue3-apexcharts"
-import VueGoogleMaps from "@fawmi/vue-google-maps"
-// @ts-ignore
-import VueVectorMap from "vuevectormap"
-import "vuevectormap/src/scss/vuevectormap.scss"
-import "jsvectormap/dist/maps/world-merc"
 
 import App from "@/App.vue"
 import router from "@/router"
@@ -26,10 +20,5 @@ const app = createApp(App)
 app.use(pinia)
 app.use(i18n)
 app.use(router)
-app.use(VueApexCharts)
-app.use(VueGoogleMaps, {
-	load: {}
-})
-app.use(VueVectorMap)
 
 app.mount("#app")
