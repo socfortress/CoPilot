b0a7eb5444e67c3e18adb081088e00e881f9149d
Davide Di Modica <webmaster.ddm@gmail.com>
2024-01-26 18:02:46 +0100

    updated connector type

--- src/types/connectors.d.ts
+++ src/types/connectors.d.ts
@@ -2,20 +2,23 @@ export interface Connector {
 	clusterHealth?: null
 	connectionSuccessful: boolean
 	connector_accepts_api_key: boolean
+	connector_accepts_host_only: boolean
 	connector_accepts_file: boolean
 	connector_accepts_username_password: boolean
+	connector_accepts_extra_data: boolean
 	connector_api_key?: string
 	connector_configured: boolean
 	connector_description?: string
 	connector_extra_data?: string
 	connector_last_updated: string
 	connector_name: string
-	connector_password: string
+	connector_password?: string
 	connector_supports?: string
 	connector_type: string
 	connector_url: string
-	connector_username: string
+	connector_username?: string
 	connector_verified: boolean
+	connector_supports: ConnectorSupports | string
 	// TODO: to verify connector_file prop
 	connector_file: string
 	id: number
@@ -23,6 +26,7 @@ export interface Connector {
 	authToken?: null
 	roles?: null
 	response?: null
+	history_logs: any[]
 }
 
 export enum ConnectorFormType {
@@ -45,4 +49,8 @@ export interface ConnectorRequestPayload {
 	connector_username?: string
 	connector_password?: string
 	connector_api_key?: string
+}
+
+export enum ConnectorSupports {
+	NotSpecified = "Not specified."
 }
