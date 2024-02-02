0a9c682946796b1929225f69f11ed8e3785bac44
Taylor <taylor.walton@socfortress.co>
2024-02-02 11:14:57 -0600

    Add MonitoringAlerts model

---
+++ backend/app/integrations/monitoring_alert/models/monitoring_alert.py
@@ -0,0 +1,16 @@
+from typing import List
+from typing import Optional
+
+from sqlalchemy import Text
+from sqlmodel import Field
+from sqlmodel import Relationship
+from sqlmodel import SQLModel
+
+
+class MonitoringAlerts(SQLModel, table=True):
+    __tablename__ = "monitoring_alerts"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    alert_id: str = Field(max_length=1024, nullable=False)
+    alert_index: str = Field(max_length=1024, nullable=False)
+    customer_code: str = Field(max_length=50, nullable=False)
+    alert_source: str = Field(max_length=1024, nullable=False)
