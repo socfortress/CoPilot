208da5ffdfa97d5ec43079ee3ffbbb9f6e4733c4
Taylor <taylor.walton@socfortress.co>
2024-02-06 14:14:18 -0600

    Update monitoring alert URL in provision service

--- backend/app/integrations/monitoring_alert/services/provision.py
+++ backend/app/integrations/monitoring_alert/services/provision.py
@@ -197,13 +197,13 @@ async def provision_wazuh_monitoring_alert(request: ProvisionMonitoringAlertRequ
     logger.info(f"Invoking provision_wazuh_monitoring_alert with request: {request.dict()}")
     notification_exists = await check_if_event_notification_exists("SEND TO COPILOT")
     if not notification_exists:
-        url_whitelisted = await check_if_url_whitelist_entry_exists(f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alerts/create")
+        url_whitelisted = await check_if_url_whitelist_entry_exists(f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alert/create")
         if not url_whitelisted:
             logger.info("Provisioning URL Whitelist")
             whitelisted_urls = await build_url_whitelisted_entries(
                 whitelist_url_model=GraylogUrlWhitelistEntryConfig(
                     id=await generate_random_id(),
-                    value=f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alerts/create",
+                    value=f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alert/create",
                     title="SEND TO COPILOT",
                     type="literal",
                 ),
@@ -215,7 +215,7 @@ async def provision_wazuh_monitoring_alert(request: ProvisionMonitoringAlertRequ
             GraylogAlertWebhookNotificationModel(
                 title="SEND TO COPILOT",
                 description="Send alert to Copilot",
-                config={"url": f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alerts/create", "type": "http-notification-v1"},
+                config={"url": f"http://{os.getenv('SERVER_IP')}:5000/monitoring_alert/create", "type": "http-notification-v1"},
             ),
         )
         logger.info(f"SEND TO COPILOT Webhook provisioned with id: {notification_id}")
