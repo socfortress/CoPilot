2f55d5c8ae2b6179b4857e4ef73725e7a06499da
Davide Di Modica <webmaster.ddm@gmail.com>
2024-02-29 16:28:04 +0100

    added Report Creation page

---
+++ frontend/src/components/reportCreation/Wizard.vue
@@ -0,0 +1,1 @@
+<template>reportCreation....</template>
---
+++ frontend/src/views/ReportCreation.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="page">
+		<ReportCreation />
+	</div>
+</template>
+
+<script setup lang="ts">
+import ReportCreation from "@/components/reportCreation/Wizard.vue"
+</script>
