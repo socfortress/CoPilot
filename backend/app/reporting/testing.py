d788f7035c078357dd11cc68d0be1b9c815ca5a0
Taylor <taylor.walton@socfortress.co>
2024-03-01 15:14:46 -0600

    Refactor code to enable browser testing using Playwright

--- backend/app/reporting/testing.py
+++ backend/app/reporting/testing.py
@@ -17,11 +17,11 @@ reporting_router = APIRouter()
 )
 async def main(session: AsyncSession = Depends(get_db)):
     await verify_license_key(session)
-    # async with async_playwright() as p:
-    #     for browser_type in [p.chromium, p.firefox, p.webkit]:
-    #         browser = await browser_type.launch()
-    #         page = await browser.new_page()
-    #         await page.goto('http://playwright.dev')
-    #         await page.screenshot(path=f'example-{browser_type.name}.png')
-    #         await browser.close()
+    async with async_playwright() as p:
+        for browser_type in [p.chromium, p.firefox, p.webkit]:
+            browser = await browser_type.launch()
+            page = await browser.new_page()
+            await page.goto('http://playwright.dev')
+            await page.screenshot(path=f'example-{browser_type.name}.png')
+            await browser.close()
     return {"message": "Report generated"}
