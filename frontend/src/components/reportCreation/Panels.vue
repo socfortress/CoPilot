6b146c34d874e79c9db3cf78bf9e76f27672e6a1
Davide Di Modica <webmaster.ddm@gmail.com>
2024-03-06 15:40:57 +0100

    updated report page

--- frontend/src/components/reportCreation/Panels.vue
+++ frontend/src/components/reportCreation/Panels.vue
@@ -1,13 +1,47 @@
 <template>
 	<div class="report-panels">
-		<div class="panels-container grid grid-cols-2 gap-6 p-6" :class="{ printing }" v-if="panelsList.length">
-			<div class="panel" v-for="panel of panelsList" :key="panel.id">
-				<img :src="'data:image/png;base64,' + panel.image" v-if="panel.image" />
-				<iframe :src="panel.url + '&theme=' + panelTheme" v-else></iframe>
-				<!--
-					<iframe :src="panel.panel_url + '&theme=light'" v-show="panelTheme === 'light'"></iframe>
-					<iframe :src="panel.panel_url + '&theme=dark'" v-show="panelTheme === 'dark'"></iframe>
-				-->
+		<div class="panels-container" v-if="panelsList.length">
+			<div
+				class="panel"
+				v-for="panel of panelsList"
+				:key="panel.id"
+				:style="panel.width ? `flex-basis:${panel.width}%` : ''"
+			>
+				<div class="toolbar">
+					<n-popover trigger="hover" overlap raw placement="right-start">
+						<template #trigger>
+							<n-button size="tiny">
+								<template #icon>
+									<Icon :name="MenuIcon"></Icon>
+								</template>
+							</n-button>
+						</template>
+						<div class="popover-input-container">
+							<div class="w-52 flex items-center gap-3 py-2 px-5">
+								<span class="font-mono w-12">{{ panel.width }}</span>
+								<n-slider :tooltip="false" v-model:value="panel.width" :min="0" :max="100" :step="10" />
+							</div>
+							<!--
+								<n-input-number
+									size="tiny"
+									v-model:value="panel.width"
+									:min="0"
+									:max="100"
+									:step="10"
+									class="max-w-32"
+								/>
+							-->
+						</div>
+					</n-popover>
+				</div>
+				<div class="content">
+					<img :src="'data:image/png;base64,' + panel.image" v-if="panel.image" />
+					<iframe :src="panel.url + '&theme=' + panelTheme" v-else></iframe>
+					<!--
+						<iframe :src="panel.panel_url + '&theme=light'" v-show="panelTheme === 'light'"></iframe>
+						<iframe :src="panel.panel_url + '&theme=dark'" v-show="panelTheme === 'dark'"></iframe>
+					-->
+				</div>
 			</div>
 		</div>
 
@@ -23,59 +57,55 @@
 </template>
 
 <script setup lang="ts">
-import { ref, nextTick, computed, toRefs } from "vue"
-import { NButton, useMessage } from "naive-ui"
+import { ref, nextTick, computed, toRefs, watch, onBeforeMount } from "vue"
+import { useMessage, NButton, NInputNumber, NSlider, NPopover } from "naive-ui"
 import type { PanelLink, PanelImage } from "@/types/reporting"
 import Icon from "@/components/common/Icon.vue"
 import { type ThemeName } from "@/types/theme.d"
 import { useThemeStore } from "@/stores/theme"
 import Api from "@/api"
-import { mockImage } from "./data"
+import { mockImage, mockLinks } from "./data"
 import { saveAs } from "file-saver"
 import html2canvas from "html2canvas"
-
-// @ts-ignore
-// import domtoimage from "dom-to-image-more"
-
-const isDirty = defineModel<boolean>("isDirty", { default: false })
 
 const props = defineProps<{
 	linksList: PanelLink[]
 }>()
 const { linksList } = toRefs(props)
 
+const MenuIcon = "carbon:overflow-menu-horizontal"
 const PrintIcon = "carbon:printer"
 const message = useMessage()
 const imagesList = ref<PanelImage[]>([])
-const printing = ref(false)
-// const panelTheme = ref<"light" | "dark">("light")
 const panelTheme = computed<ThemeName>(() => useThemeStore().themeName)
 const loadingImages = ref(false)
 const loadingPrint = ref(false)
 const loading = computed(() => loadingImages.value || loadingPrint.value)
+const panelsList = ref([])
 
-const panelsList = computed(() => {
+watch(linksList, () => {
+	createPanels()
+})
+
+onBeforeMount(() => {
+	createPanels()
+})
+
+function createPanels() {
 	const panels = []
-
-	for (const i in linksList.value) {
+	for (const i in mockLinks /*linksList.value*/) {
 		panels.push({
-			id: linksList.value[i].panel_id,
-			url: linksList.value[i].panel_url,
-			image: imagesList.value[i]?.base64_image // || mockImage
+			id: mockLinks[i].panel_id, // linksList.value[i].panel_id,
+			url: mockLinks[i].panel_url, // linksList.value[i].panel_url,
+			image: imagesList.value[i]?.base64_image || mockImage,
+			width: 50
 		})
 	}
 
-	return panels
-})
+	panelsList.value = panels
+}
 
 function print() {
-	/*
-	html2canvas(document.querySelector(".panels-container")).then(canvas => {
-		//document.body.appendChild(canvas)
-		console.log(canvas.toDataURL("image/jpeg", 0.9))
-	})
-*/
-
 	loadingPrint.value = true
 	getImages(() => {
 		nextTick(() => {
@@ -94,42 +124,6 @@ function print() {
 			}, 2000)
 		})
 	})
-
-	/*
-	printing.value = true
-	// panelTheme.value = "light"
-
-	nextTick(() => {
-		setTimeout(() => {
-			window.print()
-			// panelTheme.value = "dark"
-			printing.value = false
-		}, 2000)
-	})
-	*/
-
-	/*
-	// @ts-ignore
-	html2canvas(document.querySelector(".page"), { allowTaint: true, useCORS: true }).then(canvas => {
-		//document.body.appendChild(canvas)
-		console.log(canvas.toDataURL("image/jpeg", 0.9))
-	})
-	var node = document.querySelector(".page")
-
-	domtoimage
-		.toPng(node)
-		// @ts-ignore
-		.then(function (dataUrl) {
-			var img = new Image()
-			img.src = dataUrl
-			console.log(dataUrl)
-			document.body.appendChild(img)
-		})
-		// @ts-ignore
-		.catch(function (error) {
-			console.error("oops, something went wrong!", error)
-		})
-		*/
 }
 
 function getImages(cb?: () => void) {
@@ -163,92 +157,43 @@ function getImages(cb?: () => void) {
 		border-radius: var(--border-radius);
 		background-color: var(--bg-secondary-color);
 		border: var(--border-small-050);
+		display: flex;
+		flex-wrap: wrap;
+		padding: 1vw;
 
 		.panel {
-			border-radius: var(--border-radius);
-			background-color: var(--bg-secondary-color);
-			border: var(--border-small-050);
 			overflow: hidden;
+			flex-grow: 1;
+			min-width: 200px;
+			padding: 1vw;
+			position: relative;
+
+			.toolbar {
+				position: absolute;
+				top: 8px;
+				right: 8px;
+				backdrop-filter: blur(2px);
+			}
+			.content {
+				border-radius: var(--border-radius);
+				background-color: var(--bg-secondary-color);
+				border: var(--border-small-050);
+				overflow: hidden;
+			}
 
 			iframe {
 				width: 100%;
-				height: 500px;
+				aspect-ratio: 1;
 			}
-		}
-
-		&.printing {
-			grid-template-columns: repeat(1, minmax(0, 1fr));
-			max-width: 600px;
+			img {
+				width: 100%;
+			}
 		}
 	}
 }
-</style>
 
-<style lang="scss">
-@media print {
-	html,
-	body {
-		overflow: initial;
-	}
-	body {
-		#app {
-			height: initial;
-			background-color: white;
-
-			& > .n-config-provider {
-				& > .layout {
-					display: block;
-					height: initial;
-					background-color: white;
-
-					& > .header-bar {
-						display: none;
-					}
-					& > .main {
-						background-color: white;
-						overflow: initial;
-						height: initial;
-
-						header.toolbar {
-							display: none;
-						}
-						footer.footer {
-							display: none;
-						}
-
-						& > .n-scrollbar {
-							overflow: initial;
-							height: initial;
-
-							& > .n-scrollbar-container {
-								overflow: initial;
-								height: initial;
-							}
-						}
-
-						.page {
-							.report-wizard {
-								display: none;
-							}
-							.report-panels {
-								max-width: 600px;
-
-								.panels-container {
-									grid-template-columns: repeat(1, minmax(0, 1fr));
-
-									.panel {
-										border: 2px solid black;
-									}
-								}
-								.toolbar {
-									display: none;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+.popover-input-container {
+	background-color: var(--bg-secondary-color);
+	border-radius: var(--border-radius);
 }
 </style>
