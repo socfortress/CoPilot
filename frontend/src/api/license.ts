771356f800b9ae0d8937f771d326e0d6631b8f98
Davide Di Modica <webmaster.ddm@gmail.com>
2024-03-28 15:20:19 +0100

    added license creation form

--- frontend/src/api/license.ts
+++ frontend/src/api/license.ts
@@ -2,6 +2,12 @@ import { type FlaskBaseResponse } from "@/types/flask.d"
 import { HttpClient } from "./httpClient"
 import type { License, LicenseFeatures, LicenseKey } from "@/types/license"
 
+export interface NewLicensePayload {
+	name: string
+	email: string
+	companyName: string
+}
+
 export default {
 	getLicense() {
 		return HttpClient.get<FlaskBaseResponse & { license_key: LicenseKey }>(`/license/get_license`)
@@ -25,5 +31,15 @@ export default {
 				params: { period }
 			}
 		)
+	},
+	createLicense({ name, email, companyName }: NewLicensePayload) {
+		return HttpClient.post<FlaskBaseResponse>(`/license/create_new_key`, {
+			product_id: 24355,
+			notes: "Test Key",
+			new_customer: true,
+			name,
+			email,
+			company_name: companyName
+		})
 	}
 }
--- frontend/src/components/license/LicenseEditor.vue
+++ frontend/src/components/license/LicenseEditor.vue
@@ -1,10 +1,29 @@
 <template>
 	<n-spin :show="loading">
-		<div class="license-box" :class="{ loading: !licenseKey }">
-			<p class="flex gap-4 items-center">
+		<div class="license-box" :class="{ loading: loadingLicense }">
+			<p class="flex gap-4 items-center" v-if="loadingLicense || licenseKey">
 				<span>your license:</span>
-				<Icon :name="LoadingIcon" v-if="!licenseKey"></Icon>
+				<Icon :name="LoadingIcon" v-if="loadingLicense"></Icon>
 			</p>
+			<div v-if="!loadingLicense && !licenseKey">
+				<p class="flex gap-4 items-center" v-if="!creationEnabled">no license found</p>
+				<div class="flex items-center gap-4 mt-2">
+					<div class="actions-box flex gap-2">
+						<n-button
+							type="primary"
+							:loading="loadingCreation"
+							@click="enableCreation()"
+							v-if="!creationEnabled"
+							size="small"
+						>
+							<template #icon>
+								<Icon :name="LicenseIcon"></Icon>
+							</template>
+							Create new license
+						</n-button>
+					</div>
+				</div>
+			</div>
 			<div class="flex items-center gap-4 mt-1" v-if="!replaceEnabled && licenseKey">
 				<h3>{{ licenseKey }}</h3>
 				<div class="actions-box flex gap-2">
@@ -55,16 +74,64 @@
 				Extend
 			</n-button>
 		</div>
+
+		<div class="create-box flex flex-col gap-2" v-if="creationEnabled">
+			<n-form :label-width="80" :model="creationForm" :rules="rules" ref="formRef">
+				<div class="grid gap-2 grid-auto-flow-200">
+					<n-form-item label="Name" path="name">
+						<n-input v-model:value.trim="creationForm.name" placeholder="Input name..." clearable />
+					</n-form-item>
+					<n-form-item label="Email" path="email">
+						<n-input v-model:value.trim="creationForm.email" placeholder="Input email..." clearable />
+					</n-form-item>
+					<n-form-item label="Company Name" path="companyName">
+						<n-input
+							v-model:value.trim="creationForm.companyName"
+							placeholder="Input Company Name..."
+							clearable
+						/>
+					</n-form-item>
+				</div>
+			</n-form>
+			<div class="flex gap-2 justify-end">
+				<n-button secondary :disabled="loadingCreation" @click="resetCreation()">Reset</n-button>
+				<n-button
+					type="success"
+					:loading="loadingCreation"
+					:disabled="!isCreationFormValid"
+					@click="validateCreation()"
+				>
+					<template #icon>
+						<Icon :name="LicenseIcon"></Icon>
+					</template>
+					Create License
+				</n-button>
+			</div>
+		</div>
 	</n-spin>
 </template>
 
 <script setup lang="ts">
-import { NInput, NInputNumber, NButton, NSpin, useMessage } from "naive-ui"
+import {
+	NInput,
+	NInputNumber,
+	NButton,
+	NSpin,
+	NFormItem,
+	NForm,
+	useMessage,
+	type FormRules,
+	type FormItemRule,
+	type FormInst,
+	type FormValidationError
+} from "naive-ui"
 import Icon from "@/components/common/Icon.vue"
 import Api from "@/api"
 import { onBeforeMount, ref } from "vue"
+import isEmail from "validator/es/lib/isEmail"
 import { computed } from "vue"
 import type { LicenseKey } from "@/types/license"
+import type { NewLicensePayload } from "@/api/license"
 
 const emit = defineEmits<{
 	(e: "updated"): void
@@ -72,20 +139,59 @@ const emit = defineEmits<{
 
 const LoadingIcon = "eos-icons:loading"
 const EditIcon = "uil:edit-alt"
+const LicenseIcon = "carbon:license"
 const ExtendIcon = "majesticons:clock-plus-line"
 
+const formRef = ref<FormInst>()
 const message = useMessage()
 const loadingLicense = ref(false)
 const loadingReplace = ref(false)
 const loadingExtend = ref(false)
+const loadingCreation = ref(false)
 
 const licenseKey = ref<LicenseKey | "">("")
 const licenseKeyModel = ref<LicenseKey | "">("")
 const period = ref<number>(15)
+const creationForm = ref<NewLicensePayload>(getCreationForm())
 const replaceEnabled = ref(false)
 const extendEnabled = ref(false)
+const creationEnabled = ref(false)
 
-const loading = computed(() => loadingLicense.value || loadingReplace.value || loadingExtend.value)
+const isCreationFormValid = computed(() => {
+	if (!creationForm.value.name || !creationForm.value.email || !creationForm.value.companyName) {
+		return false
+	}
+	return true
+})
+
+const loading = computed(
+	() => loadingLicense.value || loadingReplace.value || loadingExtend.value || loadingCreation.value
+)
+
+const rules: FormRules = {
+	name: {
+		required: true,
+		message: "Please input name",
+		trigger: ["input", "blur"]
+	},
+	companyName: {
+		required: true,
+		message: "Please input company name",
+		trigger: ["input", "blur"]
+	},
+	email: {
+		required: true,
+		trigger: ["input", "blur"],
+		validator: (rule: FormItemRule, value: string) => {
+			if (!value) {
+				return new Error("Email is required")
+			}
+			if (!isEmail(value)) {
+				return new Error("The email is not formatted correctly")
+			}
+		}
+	}
+}
 
 function getLicense() {
 	loadingLicense.value = true
@@ -101,7 +207,9 @@ function getLicense() {
 			}
 		})
 		.catch(err => {
-			message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+			if (err.response.status !== 404) {
+				message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+			}
 		})
 		.finally(() => {
 			loadingLicense.value = false
@@ -157,6 +265,29 @@ function extendLicense() {
 	}
 }
 
+function createLicense() {
+	loadingCreation.value = true
+
+	Api.license
+		.createLicense(creationForm.value)
+		.then(res => {
+			if (res.data.success) {
+				resetCreation()
+				message.success(res.data?.message || "License created successfully")
+				emit("updated")
+				getLicense()
+			} else {
+				message.warning(res.data?.message || "An error occurred. Please try again later.")
+			}
+		})
+		.catch(err => {
+			message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+		})
+		.finally(() => {
+			loadingCreation.value = false
+		})
+}
+
 function resetLicense() {
 	licenseKeyModel.value = licenseKey.value
 	disableReplace()
@@ -182,6 +313,40 @@ function disableExtend() {
 
 function enableExtend() {
 	extendEnabled.value = true
+}
+
+function resetCreation() {
+	creationForm.value = getCreationForm()
+	disableCreation()
+}
+
+function disableCreation() {
+	creationEnabled.value = false
+}
+
+function enableCreation() {
+	creationEnabled.value = true
+}
+
+function getCreationForm(): NewLicensePayload {
+	return {
+		name: "",
+		email: "",
+		companyName: ""
+	}
+}
+
+function validateCreation() {
+	if (!formRef.value) return
+
+	formRef.value.validate((errors?: Array<FormValidationError>) => {
+		if (!errors) {
+			createLicense()
+		} else {
+			message.warning("You must fill in the required fields correctly.")
+			return false
+		}
+	})
 }
 
 onBeforeMount(() => {
