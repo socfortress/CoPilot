0e5da377bfa1ffa2b675122b709a52765c3f5a11
Taylor <taylor.walton@socfortress.co>
2024-04-08 15:23:21 -0500

    Remove unused code related to integration results

--- backend/app/routers/modules.py
+++ backend/app/routers/modules.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from app.integrations.modules.routes.huntress import module_huntress_router
-from app.integrations.modules.routes.results import module_results_router
 
 router = APIRouter()
 
@@ -9,10 +8,4 @@ router.include_router(
     module_huntress_router,
     prefix="/integrations/modules/huntress",
     tags=["Huntress"],
-)
-
-router.include_router(
-    module_results_router,
-    prefix="/integrations/modules/results",
-    tags=["Integration Results"],
 )
