dd8173bfbcd53bb465ce988ee6f7237f3025d56b
Taylor <taylor.walton@socfortress.co>
2024-04-08 14:42:49 -0500

    Add extra data configuration to CopilotResponse

--- backend/app/integrations/modules/schema/results.py
+++ backend/app/integrations/modules/schema/results.py
@@ -2,13 +2,13 @@ from typing import Optional
 
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import Extra
 
 
 class CopilotResponse(BaseModel):
     message: str
     success: bool
     module_name: str
-    extra_data: Optional[dict] = Field(
-        None,
-        description="The Copilot response data.",
-    )
+
+    class Config:
+        extra = Extra.allow
