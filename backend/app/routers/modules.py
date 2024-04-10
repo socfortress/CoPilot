3a66949328abd7d29dda258863dda7994116140e
Taylor <taylor.walton@socfortress.co>
2024-04-10 13:47:08 -0500

    Add Mimecast module router

--- backend/app/routers/modules.py
+++ backend/app/routers/modules.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.integrations.modules.routes.huntress import module_huntress_router
+from app.integrations.modules.routes.mimecast import module_mimecast_router
 
 router = APIRouter()
 
@@ -8,4 +9,10 @@ router.include_router(
     module_huntress_router,
     prefix="/integrations/modules/huntress",
     tags=["Huntress"],
+)
+
+router.include_router(
+    module_mimecast_router,
+    prefix="/integrations/modules/mimecast",
+    tags=["Mimecast"],
 )
