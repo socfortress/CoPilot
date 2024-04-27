a3f41ff993a3a4d854de9961d04ba32950d56d0c
Davide Di Modica <webmaster.ddm@gmail.com>
2024-04-27 17:11:14 +0200

    added Soc-PendingAlerts page

---
+++ frontend/src/views/soc/PendingAlerts.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="page">
+		Pending Alerts...
+		<SocCasesList />
+	</div>
+</template>
+
+<script setup lang="ts">
+import SocCasesList from "@/components/soc/SocCases/SocCasesList.vue"
+</script>
