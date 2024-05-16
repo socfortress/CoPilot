47c1b879dfe2bad28e913ebf4c1bc627b5d1ad1f
Davide Di Modica <webmaster.ddm@gmail.com>
2024-05-16 16:21:41 +0200

    added sca item component

--- frontend/src/components/agents/ScaItem.vue
+++ frontend/src/components/agents/ScaItem.vue
@@ -1,14 +1,54 @@
 <template>
 	<n-tabs type="line" animated :tabs-padding="24">
-		<n-tab-pane name="Details" tab="Details" display-directive="show">
-			<div class="grid gap-2 grid-auto-flow-200 p-7 pt-4" v-if="properties">
+		<n-tab-pane name="Details" tab="Details" display-directive="show" class="flex flex-col gap-4 !py-8">
+			<div class="px-7">
+				<n-card content-class="bg-secondary-color" class="overflow-hidden">
+					<div class="flex justify-between gap-8 flex-wrap">
+						<n-statistic label="Checks" :value="sca.total_checks" tabular-nums />
+						<n-statistic label="Pass" :value="sca.pass" tabular-nums />
+						<n-statistic label="Fail" :value="sca.fail" tabular-nums />
+						<n-statistic label="Invalid" :value="sca.invalid" tabular-nums />
+						<n-statistic label="Score" :value="sca.score + '%'" tabular-nums />
+					</div>
+				</n-card>
+			</div>
+			<div class="px-7">
+				<n-card content-class="bg-secondary-color" class="overflow-hidden">
+					<div class="flex justify-between gap-8 xs:!flex-row flex-col">
+						<n-statistic
+							class="grow"
+							label="Start scan"
+							:value="formatDate(sca.start_scan, dFormats.datetime).toString()"
+						/>
+						<n-statistic
+							class="grow"
+							label="End scan"
+							:value="formatDate(sca.end_scan, dFormats.datetime).toString()"
+						/>
+					</div>
+				</n-card>
+			</div>
+			<div class="grid gap-2 grid-auto-flow-200 px-7" v-if="properties">
 				<KVCard v-for="(value, key) of properties" :key="key">
 					<template #key>{{ key }}</template>
 					<template #value>
-						<template v-if="value && (key === 'end_scan' || key === 'start_scan')">
-							{{ formatDate(value, dFormats.datetime) }}
+						<template v-if="value && key === 'references'">
+							<a
+								:href="value"
+								target="_blank"
+								alt="references url"
+								rel="nofollow noopener noreferrer"
+								class="leading-6"
+							>
+								<span>
+									{{ value }}
+								</span>
+								<Icon :name="LinkIcon" :size="14" class="relative top-0.5 ml-2" />
+							</a>
 						</template>
-						<template v-else>{{ value ?? "-" }}</template>
+						<template v-else>
+							{{ value ?? "-" }}
+						</template>
 					</template>
 				</KVCard>
 			</div>
@@ -32,20 +72,22 @@
 </template>
 
 <script setup lang="ts">
-import { NTabs, NTabPane, NInput } from "naive-ui"
+import { NTabs, NTabPane, NInput, NStatistic, NCard } from "naive-ui"
 import { useSettingsStore } from "@/stores/settings"
 import { formatDate } from "@/utils"
 import { type AgentSca } from "@/types/agents.d"
 import KVCard from "@/components/common/KVCard.vue"
+import Icon from "@/components/common/Icon.vue"
 import { computed } from "vue"
-import _omit from "lodash/omit"
+import _pick from "lodash/pick"
 import "@/assets/scss/vuesjv-override.scss"
 
 const { sca } = defineProps<{ sca: AgentSca }>()
 
 const dFormats = useSettingsStore().dateFormat
+const LinkIcon = "carbon:launch"
 
 const properties = computed(() => {
-	return _omit(sca, ["description", "extract", "end_scan_text"])
+	return _pick(sca, ["name", "hash_file", "references"])
 })
 </script>
