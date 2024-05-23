d0aeadaa4d3bd803d4fae18d3609dd05670e564d
Davide Di Modica <webmaster.ddm@gmail.com>
2024-05-23 14:44:09 +0200

    added processNameEvaluation api

--- frontend/src/types/threatIntel.d.ts
+++ frontend/src/types/threatIntel.d.ts
@@ -8,3 +8,55 @@ export interface ThreatIntelResponse {
 	value: string | null
 	virustotal_url: string | null
 }
+
+export interface EvaluationData {
+	rank: number
+	host_prev: string
+	eps: string
+	paths: EvaluationDataPath[]
+	parents: EvaluationDataParent[]
+	hashes: EvaluationDataHash[]
+	network: EvaluationDataNetwork[]
+	description: string
+	intel: string
+	truncated: EvaluationDataTruncated
+	tags: EvaluationDataTag[]
+}
+
+export interface EvaluationDataHash {
+	hash: string
+	percentage: number
+}
+
+export interface EvaluationDataNetwork {
+	port: string
+	usage: number
+}
+
+export interface EvaluationDataParent {
+	name: string
+	percentage: number
+}
+
+export interface EvaluationDataPath {
+	directory: string
+	percentage: number
+}
+
+export interface EvaluationDataTag {
+	category: string
+	type: string
+	description: string
+	field4: string
+	field5: string
+	color: string
+}
+
+export interface EvaluationDataTruncated {
+	paths: number
+	parents: number
+	grandparents: number
+	children: number
+	network: number
+	hashes: number
+}
