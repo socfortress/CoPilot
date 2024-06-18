29a274919d527cdf83841d4ff0469045ef8f8e86
Davide Di Modica <webmaster.ddm@gmail.com>
2024-06-19 00:28:03 +0200

    update: cloudSecurityAssessment api/types

--- frontend/src/api/cloudSecurityAssessment.ts
+++ frontend/src/api/cloudSecurityAssessment.ts
@@ -1,6 +1,11 @@
 import { type FlaskBaseResponse } from "@/types/flask.d"
 import { HttpClient } from "./httpClient"
-import type { ScoutSuiteReportPayload, ScoutSuiteReport } from "@/types/cloudSecurityAssessment.d"
+import type {
+	ScoutSuiteAwsReportPayload,
+	ScoutSuiteAzureReportPayload,
+	ScoutSuiteReport,
+	ScoutSuiteReportPayload
+} from "@/types/cloudSecurityAssessment.d"
 
 export default {
 	getAvailableScoutSuiteReports() {
@@ -11,8 +16,14 @@ export default {
 	getScoutSuiteReportGenerationOptions() {
 		return HttpClient.get<FlaskBaseResponse & { options: string[] }>(`/scoutsuite/report-generation-options`)
 	},
-	generateAwsScoutSuiteReport(payload: ScoutSuiteReportPayload) {
+	generateAwsScoutSuiteReport(payload: ScoutSuiteReportPayload & ScoutSuiteAwsReportPayload) {
 		return HttpClient.post<FlaskBaseResponse>(`/scoutsuite/generate-aws-report`, { ...payload, report_type: "aws" })
+	},
+	generateAzureScoutSuiteReport(payload: ScoutSuiteReportPayload & ScoutSuiteAzureReportPayload) {
+		return HttpClient.post<FlaskBaseResponse>(`/scoutsuite/generate-azure-report`, {
+			...payload,
+			report_type: "azure"
+		})
 	},
 	deleteScoutSuiteReport(reportName: string) {
 		return HttpClient.delete<FlaskBaseResponse>(`/scoutsuite/delete-report/${reportName}`)
--- frontend/src/types/cloudSecurityAssessment.d.ts
+++ frontend/src/types/cloudSecurityAssessment.d.ts
@@ -1,8 +1,19 @@
 export type ScoutSuiteReport = string
 
+export enum ScoutSuiteReportType {
+	AWS = "aws",
+	Azure = "azure"
+}
 export interface ScoutSuiteReportPayload {
-	report_type: string
+	report_type: ScoutSuiteReportType
+	report_name: string
+}
+export interface ScoutSuiteAwsReportPayload {
 	access_key_id: string
 	secret_access_key: string
-	report_name: string
+}
+export interface ScoutSuiteAzureReportPayload {
+	username: string
+	password: string
+	tenant_id: string
 }
