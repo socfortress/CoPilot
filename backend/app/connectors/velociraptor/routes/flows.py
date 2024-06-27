3f6633ac695be0d1325c3e294c9788c03f51b958
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2024-06-27 14:04:12 -0500

    Velo dif org artifact collect (#257)
    * refactor: Collect Velociraptor artifacts and flows per organization
    
    * precommit fixes

--- backend/app/connectors/velociraptor/routes/flows.py
+++ backend/app/connectors/velociraptor/routes/flows.py
@@ -56,6 +56,74 @@ async def get_velociraptor_id(session: AsyncSession, hostname: str) -> str:
     return agent.velociraptor_id
 
 
+async def get_velociraptor_org(session: AsyncSession, hostname: str) -> str:
+    """
+    Retrieves the velociraptor_org associated with the given hostname.
+
+    Args:
+        session (AsyncSession): The database session.
+        hostname (str): The hostname of the agent.
+
+    Returns:
+        str: The velociraptor_org associated with the hostname.
+
+    Raises:
+        HTTPException: If the agent with the given hostname is not found or if the velociraptor_org is not available.
+    """
+    logger.info(f"Getting velociraptor_org from hostname {hostname}")
+    result = await session.execute(select(Agents).filter(Agents.hostname == hostname))
+    agent = result.scalars().first()
+
+    if not agent:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Agent with hostname {hostname} not found",
+        )
+
+    if agent.velociraptor_org is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Velociraptor ORG for hostname {hostname} is not available",
+        )
+
+    logger.info(f"velociraptor_org for hostname {hostname} is {agent.velociraptor_org}")
+    return agent.velociraptor_org
+
+
+async def get_velociraptor_org_via_client_id(session: AsyncSession, client_id: str) -> str:
+    """
+    Retrieves the velociraptor_org associated with the given hostname.
+
+    Args:
+        session (AsyncSession): The database session.
+        hostname (str): The hostname of the agent.
+
+    Returns:
+        str: The velociraptor_org associated with the hostname.
+
+    Raises:
+        HTTPException: If the agent with the given hostname is not found or if the velociraptor_org is not available.
+    """
+    logger.info(f"Getting velociraptor_org from client id {client_id}")
+    result = await session.execute(select(Agents).filter(Agents.velociraptor_id == client_id))
+    agent = result.scalars().first()
+
+    if not agent:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Agent with client id {client_id} not found",
+        )
+
+    if agent.velociraptor_org is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Velociraptor ORG for hostname {client_id} is not available",
+        )
+
+    logger.info(f"velociraptor_org for hostname {client_id} is {agent.velociraptor_org}")
+    return agent.velociraptor_org
+
+
 @velociraptor_flows_router.get(
     "/{hostname}",
     response_model=FlowResponse,
@@ -79,8 +147,12 @@ async def get_all_flows_for_hostname(
     logger.info(f"Fetching all flows for hostname {hostname}")
 
     velociraptor_id = await get_velociraptor_id(session, hostname)
+    velociraptor_org = await get_velociraptor_org(
+        session,
+        hostname,
+    )
     logger.info(f"velociraptor_id for hostname {hostname} is {velociraptor_id}")
-    return await get_flows(velociraptor_id)
+    return await get_flows(velociraptor_id, velociraptor_org)
 
 
 @velociraptor_flows_router.post(
@@ -91,6 +163,7 @@ async def get_all_flows_for_hostname(
 )
 async def retrieve_flow(
     retrieve_flow_request: RetrieveFlowRequest,
+    session: AsyncSession = Depends(get_db),
 ) -> CollectArtifactResponse:
     """
     Retrieve ran flows for a specific host.
@@ -103,4 +176,4 @@ async def retrieve_flow(
         CollectArtifactResponse: The response containing the retrieved flows.
     """
     logger.info(f"Fetching flow for flow_id {retrieve_flow_request.session_id}")
-    return await get_flow(retrieve_flow_request)
+    return await get_flow(retrieve_flow_request, await get_velociraptor_org_via_client_id(session, retrieve_flow_request.client_id))
