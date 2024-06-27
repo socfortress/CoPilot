47c7858824947a0a2559cd801ac1a17e3fa042d7
Taylor <taylor.walton@socfortress.co>
2024-06-27 17:19:09 -0500

    add epss api logic

--- backend/app/routers/threat_intel.py
+++ backend/app/routers/threat_intel.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.threat_intel.routes.socfortress import threat_intel_socfortress_router
+from app.threat_intel.routes.epss import threat_intel_epss_router
 
 # Instantiate the APIRouter
 router = APIRouter()
@@ -8,6 +9,12 @@ router = APIRouter()
 # Include the Threat Intel related routes
 router.include_router(
     threat_intel_socfortress_router,
+    prefix="/threat_intel",
+    tags=["Threat Intel"],
+)
+
+router.include_router(
+    threat_intel_epss_router,
     prefix="/threat_intel",
     tags=["Threat Intel"],
 )
