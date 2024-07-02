10b3773bd712c517dd4233763450ff103c627c1e
Taylor <taylor.walton@socfortress.co>
2024-07-02 16:44:58 -0500

    precommit fixes

--- backend/app/connectors/velociraptor/services/artifacts.py
+++ backend/app/connectors/velociraptor/services/artifacts.py
@@ -302,11 +302,10 @@ async def post_to_copilot_ai_module(data: ArtifactReccomendationRequest) -> Arti
             timeout=120,
         )
         response_data = data.json()
-        logger.info(f"Response from copilot-ai-module: {response_data}")
 
-        if not response_data.get('success'):
+        if not response_data.get("success"):
             raise HTTPException(
                 status_code=400,
-                detail=response_data.get('message', 'Request to copilot-ai-module was not successful'),
+                detail=response_data.get("message", "Request to copilot-ai-module was not successful"),
             )
     return ArtifactReccomendationResponse(**data.json())
