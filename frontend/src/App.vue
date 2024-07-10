a91e4bbe2b85d09385be01b9773120013a9d033c
Davide Di Modica <webmaster.ddm@gmail.com>
2024-07-10 18:27:03 +0200

    update: entry point component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,58 +1,56 @@
 <template>
 	<Provider>
-		<component :is="layoutComponent" :class="[`theme-${themeName}`, `layout-${layout}`, themeName]">
+		<component
+			:is="layoutComponent"
+			:class="[`theme-${themeName}`, `layout-${layoutComponentName}`, themeName, { 'opacity-0': loading }]"
+		>
 			<RouterView v-slot="{ Component }">
 				<transition :name="`router-${routerTransition}`" mode="out-in" appear>
 					<component
 						:is="Component"
 						:key="forceRefresh"
-						:class="[`theme-${themeName}`, `layout-${layout}`, themeName]"
+						:class="[`theme-${themeName}`, `layout-${layoutComponentName}`, themeName]"
 					/>
 				</transition>
 			</RouterView>
 		</component>
 
-		<SplashScreen :loading="loading" />
+		<SplashScreen :show="loading" />
 		<SearchDialog v-if="isLogged" />
-		<!--
-			<LayoutSettings />
-		-->
 	</Provider>
 </template>
 
 <script lang="ts" setup>
-import { computed, onBeforeMount, ref, type Component } from "vue"
+import { computed, onBeforeMount, ref, watch, type Component } from "vue"
 import { useMainStore } from "@/stores/main"
 import { useAuthStore } from "@/stores/auth"
 import { useThemeStore } from "@/stores/theme"
-import VerticalNav from "@/layouts/VerticalNav/index.vue"
 import HorizontalNav from "@/layouts/HorizontalNav/index.vue"
 import Blank from "@/layouts/Blank/index.vue"
 import Provider from "@/layouts/common/Provider.vue"
 import SplashScreen from "@/layouts/common/SplashScreen.vue"
-// import LayoutSettings from "@/components/common/LayoutSettings.vue"
 import SearchDialog from "@/components/common/SearchDialog.vue"
 import { Layout, RouterTransition, type ThemeName } from "@/types/theme.d"
-import { type RouteLocationNormalized, useRouter, useRoute } from "vue-router"
+import { type RouteLocationNormalized, useRoute, useRouter } from "vue-router"
 import "@/assets/scss/index.scss"
 
 const router = useRouter()
 const loading = ref(true)
 
 const layoutComponents = {
-	VerticalNav,
 	HorizontalNav,
 	Blank
 }
 
-const mainStore = useMainStore()
 const themeStore = useThemeStore()
+const mainStore = useMainStore()
 const authStore = useAuthStore()
 
-const forceLayout = ref<Layout | null>(null)
 const forceRefresh = computed<number>(() => mainStore.forceRefresh)
+const forceLayout = ref<Layout | null>(null)
 const layout = computed<Layout>(() => themeStore.layout)
-const layoutComponent = computed<Component>(() => layoutComponents[forceLayout.value || layout.value])
+const layoutComponentName = computed<Layout>(() => forceLayout.value || layout.value)
+const layoutComponent = computed<Component>(() => layoutComponents[layoutComponentName.value])
 const routerTransition = computed<RouterTransition>(() => themeStore.routerTransition)
 const themeName = computed<ThemeName>(() => themeStore.themeName)
 const isLogged = computed(() => authStore.isLogged)
@@ -65,7 +63,11 @@ function checkForcedLayout(route: RouteLocationNormalized) {
 	}
 }
 
-router.beforeEach(route => {
+watch(layoutComponentName, () => {
+	loading.value = false
+})
+
+router.afterEach(route => {
 	checkForcedLayout(route)
 })
 
