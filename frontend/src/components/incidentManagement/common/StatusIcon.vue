5526a7b081bdf0647f6e617e6d03340fd81d7a30
Davide Di Modica <webmaster.ddm@gmail.com>
2024-08-16 13:46:19 +0200

    refactor: incident management components

--- frontend/src/components/incidentManagement/common/StatusIcon.vue
+++ frontend/src/components/incidentManagement/common/StatusIcon.vue
@@ -1,6 +1,14 @@
 <template>
 	<Icon
-		:name="status === 'OPEN' ? WarningIcon : status === 'IN_PROGRESS' ? ProgressIcon : CheckIcon"
+		:name="
+			status === 'OPEN'
+				? WarningIcon
+				: status === 'IN_PROGRESS'
+					? ProgressIcon
+					: status === 'CLOSED'
+						? CheckIcon
+						: UnknownIcon
+		"
 		:size="size || 16"
 	/>
 </template>
@@ -10,10 +18,11 @@ import { toRefs } from "vue"
 import Icon from "@/components/common/Icon.vue"
 import type { AlertStatus } from "@/types/incidentManagement/alerts.d"
 
-const props = defineProps<{ status: AlertStatus; size?: number }>()
+const props = defineProps<{ status: AlertStatus | null; size?: number }>()
 const { status, size } = toRefs(props)
 
 const ProgressIcon = "carbon:hourglass"
 const CheckIcon = "carbon:checkmark-outline"
 const WarningIcon = "carbon:warning-hex"
+const UnknownIcon = "carbon:unknown"
 </script>
