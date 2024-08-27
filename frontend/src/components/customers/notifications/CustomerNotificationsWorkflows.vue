006943236e43e4570ae71803f68d7bd5b4796d87
Davide Di Modica <webmaster.ddm@gmail.com>
2024-08-27 17:08:08 +0200

    added incident notifications components

---
+++ frontend/src/components/customers/notifications/CustomerNotificationsWorkflows.vue
@@ -0,0 +1,135 @@
+<template>
+	<div class="customer-notifications-workflows">
+		<transition name="form-fade" mode="out-in">
+			<div v-if="showForm" class="p-7 pt-4">
+				<CustomerNotificationsWorkflowsForm
+					:customerCode
+					@mounted="formCTX = $event"
+					@submitted="refreshList()"
+				>
+					<template #additionalActions="{ loading }">
+						<n-button @click="closeForm()" :disabled="loading">Close</n-button>
+					</template>
+				</CustomerNotificationsWorkflowsForm>
+			</div>
+			<div v-else>
+				<n-spin :show="loading" class="min-h-48">
+					<template v-if="list.length">
+						<div class="list p-7 pt-4">
+							<CustomerNotificationsWorkflowsItem
+								v-for="item of list"
+								:key="item.id"
+								:incidentNotification="item"
+								embedded
+								@updated="getCustomerNetworkConnectors()"
+								class="item-appear item-appear-bottom item-appear-005 mb-2"
+							/>
+						</div>
+					</template>
+					<template v-else>
+						<n-empty class="justify-center h-48" v-if="!loading">
+							<div class="flex flex-col items-center gap-4">
+								<p>No Notification found</p>
+
+								<n-button size="small" @click="openForm()" type="primary">
+									<template #icon>
+										<Icon :name="AddIcon" :size="14"></Icon>
+									</template>
+									Create a Notification
+								</n-button>
+							</div>
+						</n-empty>
+					</template>
+				</n-spin>
+			</div>
+		</transition>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onBeforeMount, defineAsyncComponent } from "vue"
+import { useMessage, NSpin, NEmpty, NButton } from "naive-ui"
+import Icon from "@/components/common/Icon.vue"
+import Api from "@/api"
+import type { IncidentNotification } from "@/types/incidentManagement/notifications.d"
+
+const CustomerNotificationsWorkflowsItem = defineAsyncComponent(
+	() => import("./CustomerNotificationsWorkflowsItem.vue")
+)
+const CustomerNotificationsWorkflowsForm = defineAsyncComponent(
+	() => import("./CustomerNotificationsWorkflowsForm.vue")
+)
+
+const { customerCode } = defineProps<{
+	customerCode: string
+}>()
+
+const AddIcon = "carbon:add-alt"
+
+const message = useMessage()
+const showForm = ref(false)
+const loading = ref(false)
+const list = ref<IncidentNotification[]>([])
+const formCTX = ref<{ reset: (incidentNotification?: IncidentNotification) => void } | null>(null)
+
+function getCustomerNetworkConnectors() {
+	loading.value = true
+
+	Api.incidentManagement
+		.getNotifications(customerCode)
+		.then(res => {
+			if (res.data.success) {
+				list.value = res.data?.notifications || []
+			} else {
+				message.warning(res.data?.message || "An error occurred. Please try again later.")
+			}
+		})
+		.catch(err => {
+			message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+		})
+		.finally(() => {
+			loading.value = false
+		})
+}
+
+function openForm() {
+	showForm.value = true
+}
+
+function closeForm() {
+	showForm.value = false
+}
+
+function refreshList() {
+	closeForm()
+	getCustomerNetworkConnectors()
+}
+
+onBeforeMount(() => {
+	getCustomerNetworkConnectors()
+})
+</script>
+
+<style lang="scss" scoped>
+.customer-notifications-workflows {
+	.list {
+		container-type: inline-size;
+		min-height: 200px;
+	}
+
+	.form-fade-enter-active,
+	.form-fade-leave-active {
+		transition:
+			opacity 0.2s ease-in-out,
+			transform 0.3s ease-in-out;
+	}
+	.form-fade-enter-from {
+		opacity: 0;
+		transform: translateY(10px);
+	}
+	.form-fade-leave-to {
+		opacity: 0;
+		transform: translateY(-10px);
+	}
+}
+</style>
---
+++ frontend/src/components/customers/notifications/CustomerNotificationsWorkflowsForm.vue
@@ -0,0 +1,158 @@
+<template>
+	<div class="customer-notifications-workflows-form flex flex-col justify-between grow">
+		<div class="form-box">
+			<n-spin v-model:show="loading">
+				<n-form :label-width="80" :model="form" :rules="rules" ref="formRef">
+					<n-form-item label="Enabled" path="enabled">
+						<n-switch v-model:value="form.enabled" />
+					</n-form-item>
+					<n-form-item label="Shuffle Workflow Id" path="shuffle_workflow_id">
+						<n-input
+							v-model:value.trim="form.shuffle_workflow_id"
+							placeholder="Input the Shuffle Workflow Id..."
+							clearable
+						/>
+					</n-form-item>
+				</n-form>
+			</n-spin>
+		</div>
+		<div class="buttons-box flex justify-between gap-3">
+			<div class="flex gap-3">
+				<slot name="additionalActions" :loading></slot>
+			</div>
+			<div class="flex gap-3">
+				<n-button @click="reset()" secondary :disabled="loading">Reset</n-button>
+				<n-button type="primary" :disabled="!isValid" @click="validate()" :loading="loading">Submit</n-button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from "vue"
+import {
+	NButton,
+	NForm,
+	NFormItem,
+	NInput,
+	NSwitch,
+	NSpin,
+	useMessage,
+	type FormRules,
+	type FormInst,
+	type FormValidationError
+} from "naive-ui"
+import Api from "@/api"
+import type { IncidentNotification, IncidentNotificationPayload } from "@/types/incidentManagement/notifications.d"
+
+interface IncidentNotificationForm {
+	shuffle_workflow_id: string
+	enabled: boolean
+}
+
+const emit = defineEmits<{
+	(e: "submitted", value: IncidentNotification): void
+	(e: "startLoading"): void
+	(e: "stopLoading"): void
+	(
+		e: "mounted",
+		value: {
+			reset: () => void
+		}
+	): void
+}>()
+
+const { incidentNotification, customerCode } = defineProps<{
+	incidentNotification?: IncidentNotification
+	customerCode: string
+}>()
+
+const message = useMessage()
+const form = ref<IncidentNotificationForm>(getClearForm(incidentNotification))
+const formRef = ref<FormInst | null>(null)
+const loading = ref(false)
+
+const isValid = computed(() => {
+	return !!form.value.shuffle_workflow_id
+})
+
+const rules: FormRules = {
+	shuffle_workflow_id: {
+		required: true,
+		message: "Please insert Shuffle Workflow Id",
+		trigger: ["input", "blur"]
+	}
+}
+
+watch(loading, val => {
+	if (val) {
+		emit("startLoading")
+	} else {
+		emit("stopLoading")
+	}
+})
+
+function getClearForm(incidentNotification?: IncidentNotification): IncidentNotificationForm {
+	return {
+		shuffle_workflow_id: incidentNotification?.shuffle_workflow_id ?? "",
+		enabled: incidentNotification?.enabled ?? false
+	}
+}
+
+function reset(incidentNotification?: IncidentNotification) {
+	if (!loading.value) {
+		resetForm(incidentNotification)
+		formRef.value?.restoreValidation()
+	}
+}
+
+function resetForm(incidentNotification?: IncidentNotification) {
+	form.value = getClearForm(incidentNotification)
+}
+
+function validate() {
+	if (!formRef.value) return
+
+	formRef.value.validate((errors?: Array<FormValidationError>) => {
+		if (!errors) {
+			submit()
+		} else {
+			message.warning("You must fill in the required fields correctly.")
+			return false
+		}
+	})
+}
+
+function submit() {
+	loading.value = true
+
+	const payload: IncidentNotificationPayload = {
+		customer_code: customerCode,
+		shuffle_workflow_id: form.value.shuffle_workflow_id,
+		enabled: form.value.enabled
+	}
+
+	Api.incidentManagement
+		.setNotification(payload)
+		.then(res => {
+			if (res.data.success) {
+				message.success(res.data?.message || "Notification saved successfully")
+				emit("submitted", { ...payload, id: incidentNotification?.id || 0 })
+			} else {
+				message.warning(res.data?.message || "An error occurred. Please try again later.")
+			}
+		})
+		.catch(err => {
+			message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+		})
+		.finally(() => {
+			loading.value = false
+		})
+}
+
+onMounted(() => {
+	emit("mounted", {
+		reset
+	})
+})
+</script>
