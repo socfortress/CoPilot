ad53556adb9e6a763123ad29f8198448c17a6f51
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2024-09-03 15:12:07 -0500

    Log remove (#282)
    * feat: Update `collect_agent_vulnerabilities` to support multiple severity levels
    
    * precommit fixes

--- backend/app/agents/wazuh/services/vulnerabilities.py
+++ backend/app/agents/wazuh/services/vulnerabilities.py
@@ -9,37 +9,6 @@ from app.connectors.wazuh_indexer.utils.universal import collect_indices
 from app.connectors.wazuh_indexer.utils.universal import create_wazuh_indexer_client
 from app.connectors.wazuh_manager.utils.universal import send_get_request
 
-# async def collect_agent_vulnerabilities(agent_id: str, vulnerability_severity: str):
-#     """
-#     Collect agent vulnerabilities from Wazuh Manager.
-#     Used when Wazuh Manager is below 4.8.0
-
-#     Args:
-#         agent_id (str): The ID of the agent.
-
-#     Returns:
-#         WazuhAgentVulnerabilitiesResponse: An object containing the collected vulnerabilities.
-
-#     Raises:
-#         HTTPException: If there is an error collecting the vulnerabilities.
-#     """
-#     logger.info(f"Collecting agent {agent_id} vulnerabilities from Wazuh Manager")
-#     agent_vulnerabilities = await send_get_request(
-#         endpoint=f"/vulnerability/{agent_id}",
-#         params={"severity": vulnerability_severity},
-#     )
-#     if agent_vulnerabilities["success"] is False:
-#         raise HTTPException(status_code=500, detail=agent_vulnerabilities["message"])
-
-#     processed_vulnerabilities = process_agent_vulnerabilities(
-#         agent_vulnerabilities["data"],
-#     )
-#     return WazuhAgentVulnerabilitiesResponse(
-#         vulnerabilities=processed_vulnerabilities,
-#         success=True,
-#         message="Vulnerabilities collected successfully",
-#     )
-
 
 async def collect_agent_vulnerabilities(agent_id: str, vulnerability_severity: str):
     """
@@ -68,7 +37,6 @@ async def collect_agent_vulnerabilities(agent_id: str, vulnerability_severity: s
         )
         if response["success"] is False:
             raise HTTPException(status_code=500, detail=response["message"])
-        logger.info(f"Vulnerabilities collected for severity {severity}: {response}")
         # Navigate through the nested 'data' structure to get 'affected_items'
         affected_items = response.get("data", {}).get("data", {}).get("affected_items", [])
         agent_vulnerabilities.extend(affected_items)
@@ -81,33 +49,6 @@ async def collect_agent_vulnerabilities(agent_id: str, vulnerability_severity: s
         message="Vulnerabilities collected successfully",
     )
 
-
-# def process_agent_vulnerabilities(
-#     agent_vulnerabilities: dict,
-# ) -> List[WazuhAgentVulnerabilities]:
-#     """
-#     Process agent vulnerabilities and return a list of WazuhAgentVulnerabilities objects.
-
-#     Args:
-#         agent_vulnerabilities (dict): A dictionary containing agent vulnerabilities data.
-
-#     Returns:
-#         List[WazuhAgentVulnerabilities]: A list of WazuhAgentVulnerabilities objects.
-
-#     Raises:
-#         HTTPException: If there is an error processing the agent vulnerabilities.
-#     """
-#     try:
-#         vulnerabilities = agent_vulnerabilities.get("data", {}).get(
-#             "affected_items",
-#             [],
-#         )
-#         return [WazuhAgentVulnerabilities(**vuln) for vuln in vulnerabilities]
-#     except Exception as e:
-#         raise HTTPException(
-#             status_code=500,
-#             detail=f"Failed to process agent vulnerabilities: {e}",
-#         )
 
 def process_agent_vulnerabilities(
     agent_vulnerabilities: List[dict],
@@ -124,7 +65,6 @@ def process_agent_vulnerabilities(
     Raises:
         HTTPException: If there is an error processing the agent vulnerabilities.
     """
-    logger.info(f"Processing agent vulnerabilities: {agent_vulnerabilities}")
     try:
         return [WazuhAgentVulnerabilities(**vuln) for vuln in agent_vulnerabilities]
     except Exception as e:
@@ -132,6 +72,7 @@ def process_agent_vulnerabilities(
             status_code=500,
             detail=f"Failed to process agent vulnerabilities: {e}",
         )
+
 
 async def collect_agent_vulnerabilities_new(agent_id: str, vulnerability_severity: str):
     """
