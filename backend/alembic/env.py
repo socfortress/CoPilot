368124425fe49de0a428c67f65261c857511781e
Taylor <taylor.walton@socfortress.co>
2024-10-30 15:24:09 -0500

    `refactor: Add IoC and AlertToIoC models`

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -37,6 +37,7 @@ from app.incidents.models import Comment
 from app.incidents.models import CustomerCodeFieldName
 from app.incidents.models import FieldName
 from app.incidents.models import Notification
+from app.incidents.models import IoC, AlertToIoC
 from app.integrations.alert_creation_settings.models.alert_creation_settings import (
     AlertCreationSettings,
 )
