12737835c8de46ec1a708af5d88ee18eec8725d1
Davide Di Modica <webmaster.ddm@gmail.com>
2024-11-11 17:17:03 +0100

    fix: improved code highlighter

--- frontend/src/components/common/CodeSource.vue
+++ frontend/src/components/common/CodeSource.vue
@@ -2,13 +2,9 @@
 	<div class="flex flex-col gap-2">
 		<n-card v-if="!showSource" content-class="bg-secondary !p-0" class="overflow-hidden">
 			<div v-shiki="{ lang, decode }" class="scrollbar-styled code-bg-transparent overflow-hidden">
-				<pre v-html="source"></pre>
+				<pre v-html="code"></pre>
 			</div>
 		</n-card>
-
-		<div class="flex justify-end">
-			<n-button quaternary size="tiny" @click="showSource = !showSource">toggle source view</n-button>
-		</div>
 
 		<n-input
 			v-if="showSource"
@@ -22,6 +18,10 @@
 				maxRows: 18
 			}"
 		/>
+
+		<div v-if="showToggleButton" class="flex justify-end">
+			<n-button quaternary size="tiny" @click="showSource = !showSource">toggle source view</n-button>
+		</div>
 	</div>
 </template>
 
@@ -30,12 +30,18 @@ import vShiki from "@/directives/v-shiki"
 import { NButton, NCard, NInput } from "naive-ui"
 import { computed, ref } from "vue"
 
-const { code, lang } = defineProps<{
+const {
+	code,
+	lang,
+	decode,
+	showToggleButton = true
+} = defineProps<{
 	code: string | object | number
 	lang?: string
 	decode?: boolean
+	showToggleButton?: boolean
 }>()
 
 const showSource = ref(false)
-const source = computed(() => JSON.stringify(code, null, "\t"))
+const source = computed(() => `${code}`)
 </script>
