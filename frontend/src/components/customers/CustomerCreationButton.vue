256cd31a5bddce4faca0dbf5510053bcdf3e4d09
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2024-12-18 09:22:22 -0600

    License check improvement (#380)
    * refactor: improve mssp feature check
    
    * remove comment
    
    ---------
    
    Co-authored-by: Davide Di Modica <webmaster.ddm@gmail.com>

--- frontend/src/components/customers/CustomerCreationButton.vue
+++ frontend/src/components/customers/CustomerCreationButton.vue
@@ -1,20 +1,18 @@
 <template>
-	<LicenseFeatureCheck
-		:feature="licenseKey"
-		:disabled="licenseDisabled"
-		feedback="tooltip"
-		@response="
-			(() => {
-				licenseChecked = true
-				licenseResponse = $event
-			})()
-		"
-	>
+	<div>
+		<LicenseFeatureCheck v-if="!licenseDisabled" feature="MSSP 5" @response="isMSSP5Enabled = $event" />
+		<LicenseFeatureCheck v-if="!licenseDisabled" feature="MSSP 10" @response="isMSSP10Enabled = $event" />
+		<LicenseFeatureCheck
+			v-if="!licenseDisabled"
+			feature="MSSP Unlimited"
+			@response="isMSSPUnlimitedEnabled = $event"
+		/>
+
 		<n-button
 			:size="size || 'small'"
 			type="primary"
-			:loading="!licenseChecked && !licenseDisabled"
-			:disabled="!licenseChecked || !licenseResponse || disabled"
+			:loading="licenseLoading && !licenseDisabled"
+			:disabled="!licenseEnabled || disabled"
 			@click="showAddCustomer = true"
 		>
 			<template #icon>
@@ -22,22 +20,26 @@
 			</template>
 			<div class="flex items-center gap-2">
 				<span>Add Customer</span>
-				<Icon v-if="!licenseResponse && licenseChecked" :name="LockIcon" :size="14" />
+				<Icon v-if="!licenseEnabled && !licenseLoading" :name="LockIcon" :size="14" />
 			</div>
 		</n-button>
-	</LicenseFeatureCheck>
 
-	<n-drawer
-		v-model:show="showAddCustomer"
-		:width="500"
-		style="max-width: 90vw"
-		:trap-focus="false"
-		display-directive="show"
-	>
-		<n-drawer-content title="Add Customer" closable :native-scrollbar="false">
-			<CustomerForm :reset-on-submit="true" @mounted="customerFormCTX = $event" @submitted="emit('submitted')" />
-		</n-drawer-content>
-	</n-drawer>
+		<n-drawer
+			v-model:show="showAddCustomer"
+			:width="500"
+			style="max-width: 90vw"
+			:trap-focus="false"
+			display-directive="show"
+		>
+			<n-drawer-content title="Add Customer" closable :native-scrollbar="false">
+				<CustomerForm
+					:reset-on-submit="true"
+					@mounted="customerFormCTX = $event"
+					@submitted="emit('submitted')"
+				/>
+			</n-drawer-content>
+		</n-drawer>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -65,22 +67,27 @@ const LockIcon = "carbon:locked"
 const AddUserIcon = "carbon:user-follow"
 const customerFormCTX = ref<{ reset: () => void } | null>(null)
 const showAddCustomer = ref(false)
-
-const licenseChecked = ref(!customersCount)
-const licenseResponse = ref(!customersCount)
+const isMSSP5Enabled = ref<null | boolean>(null)
+const isMSSP10Enabled = ref<null | boolean>(null)
+const isMSSPUnlimitedEnabled = ref<null | boolean>(null)
+const licenseLoading = computed<boolean>(
+	() => isMSSP5Enabled.value === null || isMSSP10Enabled.value === null || isMSSPUnlimitedEnabled.value === null
+)
 const licenseDisabled = computed<boolean>(() => !customersCount)
-const licenseKey = computed<LicenseFeatures>(() => {
-	let key: LicenseFeatures = "MSSP 5"
-
-	if (customersCount && customersCount >= 5) {
-		key = "MSSP 10"
+const licenseEnabled = computed<boolean>(() => {
+	if (isMSSPUnlimitedEnabled.value) {
+		return true
 	}
 
-	if (customersCount && customersCount >= 10) {
-		key = "MSSP Unlimited"
+	if (isMSSP10Enabled.value && customersCount !== undefined && customersCount < 10) {
+		return true
 	}
 
-	return key
+	if (isMSSP5Enabled.value && customersCount !== undefined && customersCount < 5) {
+		return true
+	}
+
+	return false
 })
 
 watch(showAddCustomer, val => {
@@ -96,15 +103,6 @@ watch(
 		if (val) {
 			showAddCustomer.value = true
 		}
-	},
-	{ immediate: true }
-)
-
-watch(
-	licenseKey,
-	() => {
-		licenseResponse.value = !customersCount
-		licenseChecked.value = !customersCount
 	},
 	{ immediate: true }
 )
