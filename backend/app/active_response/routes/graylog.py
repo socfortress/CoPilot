7ad7d2ee52513615a4e861ad38a20c9c6c612188
taylorwalton <taylor.walton@socfortress.co>
2025-03-04 17:43:53 -0600

    feat: add Graylog integration for active response invocation and event notification schema

---
+++ backend/app/active_response/routes/graylog.py
@@ -0,0 +1,60 @@
+import json
+from pathlib import Path
+
+import aiofiles
+from fastapi import APIRouter
+from fastapi import Depends, Header
+from fastapi import HTTPException
+from fastapi import Security
+from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.active_response.schema.graylog import GraylogEventNotification
+from app.active_response.schema.active_response import InvokeActiveResponseResponse
+from app.agents.routes.agents import get_agent
+from app.auth.utils import AuthHandler
+from app.connectors.wazuh_manager.utils.universal import send_put_request
+from app.db.db_session import get_db
+
+active_response_graylog_router = APIRouter()
+
+# Function to validate the Graylog header
+async def verify_graylog_header(graylog: str = Header(None)):
+    """Verify that the request has the correct Graylog header."""
+    if graylog != "test":
+        raise HTTPException(status_code=403, detail="Invalid or missing Graylog header")
+    return graylog
+
+@active_response_graylog_router.post(
+    "/graylog/invoke",
+    response_model=InvokeActiveResponseResponse,
+    description="Invoke an active response via a Graylog Alert notification.",
+    dependencies=[Depends(verify_graylog_header)],
+)
+async def invoke_active_response_graylog_route(
+    request: GraylogEventNotification,
+) -> InvokeActiveResponseResponse:
+    """
+    This route accepts an HTTP Post from Graylog. Required fields are:
+     1. Agent ID - The ID of the agent that triggered the alert
+     2. Command - The command to execute (i.e. 'dns_block.py')
+     3. Arguments - The arguments to pass to the command such as IP addresses, domains, etc.
+
+    Args:
+        request (InvokeActiveResponseRequest): The request object containing the command, custom, arguments, and alert.
+
+    Returns:
+        InvokeActiveResponseResponse: The response object indicating the success or failure of the active response invocation.
+    """
+    logger.info("Invoking Wazuh Active Response...")
+    # Append '0' to the command - This is required for Wazuh Active Response
+    command = f"{request.event.fields.COMMAND}0"
+    # Create a dictionary with the request data
+    data_dict = {"command": command, "arguments": request.event.fields.ARGUMENTS, "alert": request.event.fields.COMMAND}
+    await send_put_request(
+        endpoint='/active-response',
+        data=json.dumps(data_dict),
+        params={"wait_for_complete": True, "agents_list": [request.event.fields.AGENT_ID]}
+    )
+
+    return InvokeActiveResponseResponse(success=True, message="Wazuh Active Response invoked successfully")
---
+++ backend/app/active_response/schema/graylog.py
@@ -0,0 +1,108 @@
+from pydantic import BaseModel, Field
+from typing import Any, Dict, List, Optional
+from datetime import datetime
+
+class ReplayInfo(BaseModel):
+    timerange_start: datetime
+    timerange_end: datetime
+    query: str
+    streams: List[str]
+    filters: List[Any] = Field(default_factory=list)
+
+class GraylogEventFields(BaseModel):
+    ALERT_ID: str
+    COMMAND: str
+    AGENT_ID: str
+    ARGUMENTS: str
+    # Allow additional fields
+    additional_fields: Dict[str, Any] = Field(default_factory=dict, alias="__extra__")
+
+    class Config:
+        extra = "allow"  # Allow extra fields
+        populate_by_name = True  # Process alias fields
+
+class GraylogEvent(BaseModel):
+    id: str
+    event_definition_type: str
+    event_definition_id: str
+    origin_context: str
+    timestamp: datetime
+    timestamp_processing: datetime
+    timerange_start: Optional[datetime] = None
+    timerange_end: Optional[datetime] = None
+    streams: List[str] = Field(default_factory=list)
+    source_streams: List[str]
+    message: str
+    source: str
+    key_tuple: List[Any] = Field(default_factory=list)
+    key: str
+    priority: int
+    scores: Dict[str, Any] = Field(default_factory=dict)
+    associated_assets: List[Any] = Field(default_factory=list)
+    alert: bool
+    fields: GraylogEventFields
+    group_by_fields: Dict[str, Any] = Field(default_factory=dict)
+    replay_info: ReplayInfo
+
+class GraylogEventNotification(BaseModel):
+    event_definition_id: str
+    event_definition_type: str
+    event_definition_title: str
+    event_definition_description: str = ""
+    job_definition_id: str
+    job_trigger_id: str
+    event: GraylogEvent
+    backlog: List[Any] = Field(default_factory=list)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "event_definition_id": "67c78b93cf26aa2045bdc2ea",
+                "event_definition_type": "aggregation-v1",
+                "event_definition_title": "ACTIVE RESPONSE WEBSERVER THREAT INTEL",
+                "event_definition_description": "",
+                "job_definition_id": "67c78befcf26aa2045bdc4e9",
+                "job_trigger_id": "67c78c0bcf26aa2045bdc5b5",
+                "event": {
+                    "id": "01JNHQP35THD48VH8601F6EMHE",
+                    "event_definition_type": "aggregation-v1",
+                    "event_definition_id": "67c78b93cf26aa2045bdc2ea",
+                    "origin_context": "urn:graylog:message:es:graylog-01_3:766b70b4-f94f-11ef-be9a-005056b6f13d",
+                    "timestamp": "2025-03-04T23:21:55.840Z",
+                    "timestamp_processing": "2025-03-04T23:26:03.450Z",
+                    "timerange_start": None,
+                    "timerange_end": None,
+                    "streams": [],
+                    "source_streams": [
+                        "679945aa7c4dd06afcef5feb",
+                        "679945a47c4dd06afcef5ef3"
+                    ],
+                    "message": "ACTIVE RESPONSE WEBSERVER THREAT INTEL",
+                    "source": "soc-grlog01",
+                    "key_tuple": [],
+                    "key": "",
+                    "priority": 2,
+                    "scores": {},
+                    "associated_assets": [],
+                    "alert": True,
+                    "fields": {
+                        "ALERT_ID": "alert-123456",
+                        "COMMAND": "dns_block.py",
+                        "AGENT_ID": "032",
+                        "ARGUMENTS": "malicious-domain.com"
+                    },
+                    "group_by_fields": {},
+                    "replay_info": {
+                        "timerange_start": "2025-03-04T23:21:03.360Z",
+                        "timerange_end": "2025-03-04T23:26:03.360Z",
+                        "query": "_exists_:threat_intel_score",
+                        "streams": [
+                            "679945aa7c4dd06afcef5feb",
+                            "679945a47c4dd06afcef5ef3"
+                        ],
+                        "filters": []
+                    }
+                },
+                "backlog": []
+            }
+        }
