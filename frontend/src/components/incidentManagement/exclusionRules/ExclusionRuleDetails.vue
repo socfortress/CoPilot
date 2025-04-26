8bdec3b5e020dd769e871002d505b45d88cfdd77
Davide Di Modica <webmaster.ddm@gmail.com>
2025-04-26 16:14:21 +0200

    feat: overview component

--- frontend/src/components/incidentManagement/exclusionRules/ExclusionRuleDetails.vue
+++ frontend/src/components/incidentManagement/exclusionRules/ExclusionRuleDetails.vue
@@ -1,37 +1,114 @@
 <template>
 	<n-tabs type="line" animated :tabs-padding="24" class="grow" pane-wrapper-class="flex grow flex-col">
 		<n-tab-pane name="Overview" tab="Overview" display-directive="show:lazy" class="flex grow flex-col">
-			<div class="p-5 pt-3">
-				<div class="flex items-stretch gap-4">
-					<CardKV size="lg" class="basis-1/2">
-						<template #key>Status</template>
-						<template #value>switch...</template>
-					</CardKV>
-					<CardKV size="lg" class="basis-1/2">
-						<template #key>Creator</template>
-						<template #value>
-							<div class="flex flex-col gap-2">
-								<div>by: {{ entity.created_by }}</div>
-								<div>at: {{ formatDate(entity.created_at, dFormats.datetimesec) }}</div>
-							</div>
-						</template>
+			<n-spin :show="loading" class="flex grow flex-col" content-class="flex grow flex-col">
+				<div class="flex flex-col gap-4 p-5 pt-3">
+					<div class="grid grid-cols-6 gap-4">
+						<CardKV class="col-span-6 md:col-span-2">
+							<template #key>name</template>
+							<template #value>{{ entity.name }}</template>
+						</CardKV>
+						<CardKV class="col-span-3 md:col-span-2">
+							<template #key>creator</template>
+							<template #value>
+								<div class="flex flex-col gap-2 py-1">
+									<div>
+										<span class="text-secondary">by:</span>
+										{{ entity.created_by }}
+									</div>
+									<div>
+										<span class="text-secondary">at:</span>
+										{{ formatDate(entity.created_at, dFormats.datetimesec) }}
+									</div>
+								</div>
+							</template>
+						</CardKV>
+						<CardKV class="col-span-3 md:col-span-2">
+							<template #key>status</template>
+							<template #value>
+								<div class="flex h-full w-full items-center justify-center font-sans">
+									<ExclusionRuleStatusToggler
+										:entity
+										@loading="updatingStatus = $event"
+										@updated="setStatus($event)"
+									/>
+								</div>
+							</template>
+						</CardKV>
+					</div>
+
+					<div class="flex flex-wrap items-center gap-3">
+						<Badge type="splitted">
+							<template #label># ID</template>
+							<template #value>{{ entity.id }}</template>
+						</Badge>
+
+						<Badge type="splitted" color="primary">
+							<template #iconLeft>
+								<Icon :name="TargetIcon" />
+							</template>
+							<template #label>Match count</template>
+							<template #value>
+								{{ entity.match_count }}
+							</template>
+						</Badge>
+
+						<Badge v-if="entity.last_matched_at" type="splitted" color="primary">
+							<template #iconLeft>
+								<Icon :name="TimeIcon" />
+							</template>
+							<template #label>Last match</template>
+							<template #value>
+								{{ formatDate(entity.last_matched_at, dFormats.datetimesec) }}
+							</template>
+						</Badge>
+
+						<Badge v-if="entity.customer_code" type="splitted">
+							<template #label>Customer</template>
+							<template #value>
+								<code
+									class="text-primary cursor-pointer leading-none"
+									@click.stop="gotoCustomer({ code: entity.customer_code })"
+								>
+									#{{ entity.customer_code }}
+									<Icon :name="LinkIcon" :size="14" class="relative top-0.5" />
+								</code>
+							</template>
+						</Badge>
+					</div>
+
+					<CardKV v-for="(val, key) in properties" :key>
+						<template #key>{{ key }}</template>
+						<template #value>{{ val }}</template>
 					</CardKV>
 				</div>
-			</div>
+			</n-spin>
 		</n-tab-pane>
 		<n-tab-pane name="Fields" tab="Fields" display-directive="show:lazy">
-			<div class="p-7 pt-4">Fields...</div>
+			<div class="p-7 pt-4">
+				<CardKV v-for="(val, key) in entity.field_matches" :key size="lg">
+					<template #key>{{ key }}</template>
+					<template #value>
+						<CodeSource :code="val" lang="shell" />
+					</template>
+				</CardKV>
+			</div>
 		</n-tab-pane>
 	</n-tabs>
 </template>
 
 <script setup lang="ts">
 import type { ExclusionRule } from "@/types/incidentManagement/exclusionRules.d"
+import Badge from "@/components/common/Badge.vue"
 import CardKV from "@/components/common/cards/CardKV.vue"
+import Icon from "@/components/common/Icon.vue"
+import { useGoto } from "@/composables/useGoto"
 import { useSettingsStore } from "@/stores/settings"
 import { formatDate } from "@/utils"
-import { NTabPane, NTabs } from "naive-ui"
-import { toRefs } from "vue"
+import _pick from "lodash/pick"
+import { NSpin, NTabPane, NTabs } from "naive-ui"
+import { computed, defineAsyncComponent, ref, toRefs } from "vue"
+import ExclusionRuleStatusToggler from "./ExclusionRuleStatusToggler.vue"
 
 const props = defineProps<{
 	entity: ExclusionRule
@@ -39,16 +116,18 @@ const props = defineProps<{
 
 const { entity } = toRefs(props)
 
+const CodeSource = defineAsyncComponent(() => import("@/components/common/CodeSource.vue"))
+
+const TimeIcon = "carbon:time"
+const LinkIcon = "carbon:launch"
+const TargetIcon = "zondicons:target"
 const dFormats = useSettingsStore().dateFormat
+const { gotoCustomer } = useGoto()
+const updatingStatus = ref(false)
+const loading = computed(() => updatingStatus.value)
+const properties = computed(() => _pick(entity.value, ["description", "channel", "title"]))
 
-/*
-const emit = defineEmits<{
-	(e: "deleted", value: ExclusionRule): void
-	(e: "updated", value: ExclusionRule): void
-}>()
-
-function updateQuery(updatedQuery: SigmaQuery) {
-	emit("updated", updatedQuery)
-	}
-	*/
+function setStatus(value: ExclusionRule) {
+	entity.value.enabled = value.enabled
+}
 </script>
---
+++ frontend/src/components/incidentManagement/exclusionRules/ExclusionRuleStatusToggler.vue
@@ -0,0 +1,76 @@
+<template>
+	<n-switch
+		v-model:value="entity.enabled"
+		:rail-style
+		:loading="updatingStatus"
+		@update:value="toggleExclusionRuleStatus()"
+	>
+		<template #checked>Enabled</template>
+		<template #unchecked>Disabled</template>
+	</n-switch>
+</template>
+
+<script setup lang="ts">
+import type { ExclusionRule } from "@/types/incidentManagement/exclusionRules.d"
+import type { CSSProperties } from "vue"
+import Api from "@/api"
+import { useThemeStore } from "@/stores/theme"
+import { NSwitch, useMessage } from "naive-ui"
+import { computed, ref, toRefs, watch } from "vue"
+
+const props = defineProps<{
+	entity: ExclusionRule
+}>()
+
+const emit = defineEmits<{
+	(e: "loading", value: boolean): void
+	(e: "updated", value: ExclusionRule): void
+}>()
+
+const { entity } = toRefs(props)
+
+const message = useMessage()
+const themeStore = useThemeStore()
+const checkedColor = computed(() => themeStore.style["success-color-rgb"])
+const uncheckedColor = computed(() => themeStore.style["border-color-rgb"])
+const updatingStatus = ref(false)
+
+function railStyle({ focused, checked }: { focused: boolean; checked: boolean }) {
+	const style: CSSProperties = {}
+	if (checked) {
+		style.background = `rgb(${checkedColor.value})`
+		if (focused) {
+			style.boxShadow = `0 0 0 2px rgb(${checkedColor.value} / 30%)`
+		}
+	} else {
+		style.background = `rgb(${uncheckedColor.value})`
+		if (focused) {
+			style.boxShadow = `0 0 0 2px rgb(${uncheckedColor.value} / 30%)`
+		}
+	}
+	return style
+}
+
+function toggleExclusionRuleStatus() {
+	updatingStatus.value = true
+
+	Api.incidentManagement.exclusionRules
+		.toggleExclusionRuleStatus(entity.value.id)
+		.then(res => {
+			if (res.data.success) {
+				entity.value.enabled = res.data.exclusion_response.enabled
+				emit("updated", res.data.exclusion_response)
+			}
+		})
+		.catch(err => {
+			message.error(err.response?.data?.message || "An error occurred. Please try again later.")
+		})
+		.finally(() => {
+			updatingStatus.value = false
+		})
+}
+
+watch(updatingStatus, val => {
+	emit("loading", val)
+})
+</script>
