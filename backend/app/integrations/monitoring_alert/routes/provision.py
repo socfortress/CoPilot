8db8262e3a61330be46afb703d5f2d90fddab053
taylorwalton <taylor.walton@socfortress.co>
2025-05-06 08:05:35 -0500

    Add provisioning for Fortinet system monitoring alerts

--- backend/app/integrations/monitoring_alert/routes/provision.py
+++ backend/app/integrations/monitoring_alert/routes/provision.py
@@ -25,7 +25,7 @@ from app.integrations.monitoring_alert.schema.provision import (
 from app.integrations.monitoring_alert.schema.provision import (
     ProvisionWazuhMonitoringAlertResponse,
 )
-from app.integrations.monitoring_alert.services.provision import provision_custom_alert, provision_crowdstrike_monitoring_alert
+from app.integrations.monitoring_alert.services.provision import provision_custom_alert, provision_crowdstrike_monitoring_alert, provision_fortinet_system_monitoring_alert
 from app.integrations.monitoring_alert.services.provision import (
     provision_office365_exchange_online_alert,
 )
@@ -126,6 +126,12 @@ async def invoke_provision_crowdstrike_monitoring_alert(
     # Provision the CrowdStrike monitoring alert
     await provision_crowdstrike_monitoring_alert(request)
 
+async def invoke_provision_fortinet_system_monitoring_alert(
+    request: ProvisionMonitoringAlertRequest,
+):
+    # Provision the Fortinet System monitoring alert
+    await provision_fortinet_system_monitoring_alert(request)
+
 
 async def invoke_provision_custom_monitoring_alert(
     request: CustomMonitoringAlertProvisionModel,
@@ -141,6 +147,7 @@ PROVISION_FUNCTIONS = {
     "OFFICE365_EXCHANGE_ONLINE": invoke_provision_office365_exchange_online_alert,
     "OFFICE365_THREAT_INTEL": invoke_provision_office365_threat_intel_alert,
     "CROWDSTRIKE_ALERT": invoke_provision_crowdstrike_monitoring_alert,
+    "FORTINET_SYSTEM": invoke_provision_fortinet_system_monitoring_alert,
     "CUSTOM": invoke_provision_custom_monitoring_alert,
     # Add more alert names and functions as needed
 }
--- backend/app/integrations/monitoring_alert/schema/provision.py
+++ backend/app/integrations/monitoring_alert/schema/provision.py
@@ -38,6 +38,11 @@ class AvailableMonitoringAlerts(str, Enum):
         "alert that is created within CoPilot. Ensure that you have a pipeline rule that sets the "
         "alert_severity field to 1 when the CrowdStrike alert is detected."
     )
+    FORTINET_SYSTEM = (
+        "This alert monitors the Fortinet System events. When an alert is detected, it triggers an "
+        "alert that is created within CoPilot. Ensure that you have a pipeline rule that sets the "
+        "alert_severity field to 1 when the Fortinet alert is detected."
+    )
 
 
 class AvailableMonitoringAlertsResponse(BaseModel):
