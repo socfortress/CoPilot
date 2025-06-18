cbfbfcb14fc7f01385e3d66be64e71e95508ec72
taylorwalton <taylor.walton@socfortress.co>
2025-06-18 18:18:37 -0500

    testing for opencti

--- backend/app/connectors/shuffle/services/singul.py
+++ backend/app/connectors/shuffle/services/singul.py
@@ -5,6 +5,44 @@ from app.connectors.shuffle.utils.universal import get_shuffle_org_id
 from app.connectors.shuffle.utils.universal import get_singul_client
 
 
+# async def execute_singul(
+#     request: SingulRequest,
+# ) -> dict:
+#     """
+#     Execute a Singul integration.
+
+#     Args:
+#         request (SingulRequest): The request object containing the workflow ID.
+
+#     Returns:
+#         dict: The response containing the execution ID.
+#     """
+#     logger.info("Executing Singul integration")
+
+#     # Get Singul client from database credentials
+#     singul = await get_singul_client()
+
+#     try:
+#         response = singul.communication.send_message(
+#             app=request.app,
+#             org_id=await get_shuffle_org_id(),
+#             fields=[
+#                 {"key": "to", "value": "walton.taylor23@gmail.com"},
+#                 {"key": "subject", "value": "Test Email from Singul"},
+#                 {"key": "body", "value": "This is a test email sent from Singul."},
+#             ],
+#         )
+#         logger.info(f"Singul response: {response}")
+#         logger.info(f"Singul response success: {response.get('success', 'unknown')}")
+
+#         return {
+#             "executionId": response.get("id", "unknown"),
+#             "message": "Singul integration executed successfully",
+#         }
+#     except Exception as e:
+#         logger.error(f"Failed to execute Singul integration: {e}")
+#         return {"executionId": "unknown", "message": f"Singul integration failed: {e}", "success": False}
+
 async def execute_singul(
     request: SingulRequest,
 ) -> dict:
@@ -23,13 +61,11 @@ async def execute_singul(
     singul = await get_singul_client()
 
     try:
-        response = singul.communication.send_message(
-            app=request.app,
+        response = singul.intel.get_ioc(
+            app='opencti_dcon',
             org_id=await get_shuffle_org_id(),
             fields=[
-                {"key": "to", "value": "walton.taylor23@gmail.com"},
-                {"key": "subject", "value": "Test Email from Singul"},
-                {"key": "body", "value": "This is a test email sent from Singul."},
+                {"key": "ip", "value": "8.8.8.8"}
             ],
         )
         logger.info(f"Singul response: {response}")
