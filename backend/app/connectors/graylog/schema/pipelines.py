fb9b4227c3b56e0f1374158645bdf791ffab4bea
taylorwalton <taylor.walton@socfortress.co>
2025-07-23 15:40:41 -0500

    fix: make modified_at field optional in Pipeline and PipelineRule models

--- backend/app/connectors/graylog/schema/pipelines.py
+++ backend/app/connectors/graylog/schema/pipelines.py
@@ -19,7 +19,7 @@ class Pipeline(BaseModel):
     description: Optional[str] = None  # Make description optional
     errors: Optional[None]
     id: str
-    modified_at: str
+    modified_at: Optional[str] = None  # Make modified_at optional
     source: str
     stages: List[Stage]
     title: str
@@ -36,7 +36,7 @@ class PipelineRule(BaseModel):
     description: Optional[str] = None  # Make description optional
     errors: Optional[None]
     id: str
-    modified_at: str
+    modified_at: Optional[str] = None # Make modified_at optional
     source: str
     title: str
 
