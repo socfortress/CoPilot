0e98469f124fc2eafea4488eba2d795fd1cc4513
taylor_socfortress <111797488+taylorwalton@users.noreply.github.com>
2025-09-11 14:38:18 -0500

    Sca overview backend (#507)
    * Add SCA routes and implement overview and statistics endpoints
    
    * precommit-fixes

---
+++ backend/app/agents/sca/schema/__init__.py
@@ -0,0 +1,1 @@
+# SCA Schema Package
--- backend/app/routers/agents.py
+++ backend/app/routers/agents.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.agents.routes.agents import agents_router
+from app.agents.sca.routes.sca import sca_router
 from app.agents.vulnerabilities.routes.vulnerabilities import vulnerabilities_router
 
 # Instantiate the APIRouter
@@ -9,3 +10,4 @@ router = APIRouter()
 # Include the Wazuh Manager related routes
 router.include_router(agents_router, prefix="/agents", tags=["agents"])
 router.include_router(vulnerabilities_router, prefix="/vulnerabilities", tags=["vulnerabilities"])
+router.include_router(sca_router, prefix="/sca", tags=["sca"])
