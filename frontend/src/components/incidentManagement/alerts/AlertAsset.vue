cfc245425af211bbe8d0d31dd4c30224f7c966a0
taylorwalton <taylor.walton@socfortress.co>
2025-12-15 09:21:38 -0600

    linit clean

--- frontend/src/components/incidentManagement/alerts/AlertAsset.vue
+++ frontend/src/components/incidentManagement/alerts/AlertAsset.vue
@@ -1,172 +1,172 @@
 <template>
-    <div>
-        <div v-if="badge" class="alert-assets-badge" @click="showDetails = true">
-            <code>
-                <span>{{ asset.asset_name }}</span>
-                <Icon :name="ViewIcon" :size="14" />
-            </code>
-        </div>
-        <CardEntity v-else :embedded hoverable clickable @click="showDetails = true">
-            <template #default>{{ asset.asset_name }}</template>
-            <template #mainExtra>
-                <div class="flex flex-wrap items-center gap-3">
-                    <Badge type="splitted">
-                        <template #label>Index</template>
-                        <template #value>
-                            <div class="flex h-full items-center">
-                                <code
-                                    class="text-primary cursor-pointer leading-none"
-                                    @click.stop="gotoIndex(asset.index_name)"
-                                >
-                                    {{ asset.index_name }}
-                                    <Icon :name="LinkIcon" :size="14" class="relative top-0.5" />
-                                </code>
-                            </div>
-                        </template>
-                    </Badge>
+	<div>
+		<div v-if="badge" class="alert-assets-badge" @click="showDetails = true">
+			<code>
+				<span>{{ asset.asset_name }}</span>
+				<Icon :name="ViewIcon" :size="14" />
+			</code>
+		</div>
+		<CardEntity v-else :embedded hoverable clickable @click="showDetails = true">
+			<template #default>{{ asset.asset_name }}</template>
+			<template #mainExtra>
+				<div class="flex flex-wrap items-center gap-3">
+					<Badge type="splitted">
+						<template #label>Index</template>
+						<template #value>
+							<div class="flex h-full items-center">
+								<code
+									class="text-primary cursor-pointer leading-none"
+									@click.stop="gotoIndex(asset.index_name)"
+								>
+									{{ asset.index_name }}
+									<Icon :name="LinkIcon" :size="14" class="relative top-0.5" />
+								</code>
+							</div>
+						</template>
+					</Badge>
 
-                    <Badge type="splitted">
-                        <template #label>Agent</template>
-                        <template #value>
-                            <div class="flex h-full items-center">
-                                <code
-                                    class="text-primary cursor-pointer leading-none"
-                                    @click.stop="gotoAgent(asset.agent_id)"
-                                >
-                                    {{ asset.agent_id }}
-                                    <Icon :name="LinkIcon" :size="14" class="relative top-0.5" />
-                                </code>
-                            </div>
-                        </template>
-                    </Badge>
-                </div>
-            </template>
-        </CardEntity>
+					<Badge type="splitted">
+						<template #label>Agent</template>
+						<template #value>
+							<div class="flex h-full items-center">
+								<code
+									class="text-primary cursor-pointer leading-none"
+									@click.stop="gotoAgent(asset.agent_id)"
+								>
+									{{ asset.agent_id }}
+									<Icon :name="LinkIcon" :size="14" class="relative top-0.5" />
+								</code>
+							</div>
+						</template>
+					</Badge>
+				</div>
+			</template>
+		</CardEntity>
 
-        <n-modal
-            v-model:show="showDetails"
-            preset="card"
-            content-class="p-0!"
-            :style="{ maxWidth: 'min(800px, 90vw)', minHeight: 'min(550px, 90vh)', overflow: 'hidden' }"
-            :bordered="false"
-            :title="assetNameTruncated"
-            segmented
-        >
-            <LicenseFeatureCheck
-                feature="SOCFORTRESS AI"
-                @response="
-                    (() => {
-                        licenseChecked = true
-                        licenseResponse = $event
-                    })()
-                "
-            />
-            <n-spin :show="!licenseChecked" content-class="flex flex-wrap justify-end gap-3 p-6" :size="18">
-                <AIVelociraptorArtifactRecommendationButton
-                    :index-id="asset.index_id"
-                    :index-name="asset.index_name"
-                    :agent-id="asset.agent_id"
-                    :alert-id="asset.alert_linked"
-                    :force-license-response="licenseResponse"
-                />
-                <AIWazuhExclusionRuleButton
-                    :index-id="asset.index_id"
-                    :index-name="asset.index_name"
-                    :alert-id="asset.alert_linked"
-                    :force-license-response="licenseResponse"
-                />
-                <AIAnalystButton
-                    :index-id="asset.index_id"
-                    :index-name="asset.index_name"
-                    :alert-id="asset.alert_linked"
-                    :force-license-response="licenseResponse"
-                />
-            </n-spin>
+		<n-modal
+			v-model:show="showDetails"
+			preset="card"
+			content-class="p-0!"
+			:style="{ maxWidth: 'min(800px, 90vw)', minHeight: 'min(550px, 90vh)', overflow: 'hidden' }"
+			:bordered="false"
+			:title="assetNameTruncated"
+			segmented
+		>
+			<LicenseFeatureCheck
+				feature="SOCFORTRESS AI"
+				@response="
+					(() => {
+						licenseChecked = true
+						licenseResponse = $event
+					})()
+				"
+			/>
+			<n-spin :show="!licenseChecked" content-class="flex flex-wrap justify-end gap-3 p-6" :size="18">
+				<AIVelociraptorArtifactRecommendationButton
+					:index-id="asset.index_id"
+					:index-name="asset.index_name"
+					:agent-id="asset.agent_id"
+					:alert-id="asset.alert_linked"
+					:force-license-response="licenseResponse"
+				/>
+				<AIWazuhExclusionRuleButton
+					:index-id="asset.index_id"
+					:index-name="asset.index_name"
+					:alert-id="asset.alert_linked"
+					:force-license-response="licenseResponse"
+				/>
+				<AIAnalystButton
+					:index-id="asset.index_id"
+					:index-name="asset.index_name"
+					:alert-id="asset.alert_linked"
+					:force-license-response="licenseResponse"
+				/>
+			</n-spin>
 
-            <n-divider class="my-0!" />
+			<n-divider class="my-0!" />
 
-            <n-tabs type="line" animated :tabs-padding="24">
-                <n-tab-pane name="Info" tab="Info" display-directive="show">
-                    <AlertAssetInfo :asset />
-                </n-tab-pane>
-                <n-tab-pane name="Context" tab="Context" display-directive="show">
-                    <n-spin :show="loading" class="min-h-40">
-                        <div v-if="alertContext" class="p-7 pt-4">
-                            <div class="mb-4 flex flex-wrap gap-3">
-                                <Badge type="splitted">
-                                    <template #label>id</template>
-                                    <template #value>#{{ alertContext.id }}</template>
-                                </Badge>
-                                <Badge type="splitted">
-                                    <template #label>source</template>
-                                    <template #value>
-                                        {{ alertContext.source }}
-                                    </template>
-                                </Badge>
-                            </div>
+			<n-tabs type="line" animated :tabs-padding="24">
+				<n-tab-pane name="Info" tab="Info" display-directive="show">
+					<AlertAssetInfo :asset />
+				</n-tab-pane>
+				<n-tab-pane name="Context" tab="Context" display-directive="show">
+					<n-spin :show="loading" class="min-h-40">
+						<div v-if="alertContext" class="p-7 pt-4">
+							<div class="mb-4 flex flex-wrap gap-3">
+								<Badge type="splitted">
+									<template #label>id</template>
+									<template #value>#{{ alertContext.id }}</template>
+								</Badge>
+								<Badge type="splitted">
+									<template #label>source</template>
+									<template #value>
+										{{ alertContext.source }}
+									</template>
+								</Badge>
+							</div>
 
-                            <CodeSource :code="alertContext.context" lang="json" />
-                        </div>
-                    </n-spin>
-                </n-tab-pane>
-                <n-tab-pane
-                    v-if="isInvestigationAvailable"
-                    name="Investigate"
-                    tab="Investigate"
-                    display-directive="show:lazy"
-                >
-                    <div class="p-7 pt-4">
-                        <div class="flex flex-wrap gap-2">
-                            <ThreatIntelProcessEvaluationProvider
-                                v-for="pn of processNameList"
-                                :key="pn"
-                                v-slot="{ openEvaluation }"
-                                :process-name="pn"
-                            >
-                                <n-card
-                                    size="small"
-                                    embedded
-                                    class="hover:border-primary cursor-pointer overflow-hidden"
-                                    @click="openEvaluation()"
-                                >
-                                    {{ pn }}
-                                </n-card>
-                            </ThreatIntelProcessEvaluationProvider>
-                        </div>
-                    </div>
-                </n-tab-pane>
-                <n-tab-pane v-if="isWazuhSource" name="Artifact Collection" tab="Artifact Collection" display-directive="show:lazy">
-                    <div class="p-7 pt-2">
-                        <ArtifactsCollect
-                            :hostname="asset.asset_name"
-                            :artifacts-filter="{ hostname: asset.asset_name }"
-                            hide-hostname-field
-                            velociraptor-id="string"
-                            hide-velociraptor-id-field
-                        />
-                    </div>
-                </n-tab-pane>
-                <n-tab-pane v-if="isWazuhSource" name="Alert Timeline" tab="Alert Timeline" display-directive="show:lazy">
-                    <div class="p-7 pt-2">
-                        <AlertDetailTimeline :asset />
-                    </div>
-                </n-tab-pane>
-                <n-tab-pane v-if="isWazuhSource" name="File Collection" tab="File Collection" display-directive="show:lazy">
-                    <div class="p-7 pt-2">
-                        <FileCollectionForm v-if="asset.agent_id" :agent-id="asset.agent_id" />
+							<CodeSource :code="alertContext.context" lang="json" />
+						</div>
+					</n-spin>
+				</n-tab-pane>
+				<n-tab-pane
+					v-if="isInvestigationAvailable"
+					name="Investigate"
+					tab="Investigate"
+					display-directive="show:lazy"
+				>
+					<div class="p-7 pt-4">
+						<div class="flex flex-wrap gap-2">
+							<ThreatIntelProcessEvaluationProvider
+								v-for="pn of processNameList"
+								:key="pn"
+								v-slot="{ openEvaluation }"
+								:process-name="pn"
+							>
+								<n-card
+									size="small"
+									embedded
+									class="hover:border-primary cursor-pointer overflow-hidden"
+									@click="openEvaluation()"
+								>
+									{{ pn }}
+								</n-card>
+							</ThreatIntelProcessEvaluationProvider>
+						</div>
+					</div>
+				</n-tab-pane>
+				<n-tab-pane v-if="isWazuhSource" name="Artifact Collection" tab="Artifact Collection" display-directive="show:lazy">
+					<div class="p-7 pt-2">
+						<ArtifactsCollect
+							:hostname="asset.asset_name"
+							:artifacts-filter="{ hostname: asset.asset_name }"
+							hide-hostname-field
+							velociraptor-id="string"
+							hide-velociraptor-id-field
+						/>
+					</div>
+				</n-tab-pane>
+				<n-tab-pane v-if="isWazuhSource" name="Alert Timeline" tab="Alert Timeline" display-directive="show:lazy">
+					<div class="p-7 pt-2">
+						<AlertDetailTimeline :asset />
+					</div>
+				</n-tab-pane>
+				<n-tab-pane v-if="isWazuhSource" name="File Collection" tab="File Collection" display-directive="show:lazy">
+					<div class="p-7 pt-2">
+						<FileCollectionForm v-if="asset.agent_id" :agent-id="asset.agent_id" />
 
-                        <n-empty v-else description="No agent associated with this asset" class="h-40" />
-                    </div>
-                </n-tab-pane>
-                <n-tab-pane v-if="isWazuhSource" name="Data Store" tab="Data Store" display-directive="show:lazy">
-                    <div class="p-7 pt-2">
-                        <AgentDataStoreTabCompact v-if="asset.agent_id" :agent-id="asset.agent_id" />
-                        <n-empty v-else description="No agent associated with this asset" class="h-40" />
-                    </div>
-                </n-tab-pane>
-            </n-tabs>
-        </n-modal>
-    </div>
+						<n-empty v-else description="No agent associated with this asset" class="h-40" />
+					</div>
+				</n-tab-pane>
+				<n-tab-pane v-if="isWazuhSource" name="Data Store" tab="Data Store" display-directive="show:lazy">
+					<div class="p-7 pt-2">
+						<AgentDataStoreTabCompact v-if="asset.agent_id" :agent-id="asset.agent_id" />
+						<n-empty v-else description="No agent associated with this asset" class="h-40" />
+					</div>
+				</n-tab-pane>
+			</n-tabs>
+		</n-modal>
+	</div>
 </template>
 
 <script setup lang="ts">
