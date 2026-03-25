0f187cd9aac380ce41630aa8f65eb7d178b28161
Davide Di Modica <webmaster.ddm@gmail.com>
2026-03-25 22:09:38 +0100

    feat: add new composables for enhanced functionality
    - Introduce `useBreadcrumb` for breadcrumb management with push, replace, update, pop, clear, setItems, and truncate functionalities.
    - Implement `useCustomerHealthcheckFilters` for managing healthcheck filter settings.
    - Create `useFullscreenSwitch` to toggle fullscreen mode.
    - Add `useGlobalActions` for centralized message and notification handling.
    - Develop `useHealthchecksNotify` to manage healthcheck notifications based on store state.
    - Implement `useHideLayoutFooter` to conditionally hide the layout footer based on theme settings.
    - Create `useLoadingBarSetup` for loading bar management during route transitions.
    - Introduce `useNavigation` for simplified routing and navigation actions.
    - Add `useNotifications` for managing notification state and actions.
    - Implement `useSearchDialog` for triggering search dialogs.
    - Create `useThemeSwitch` for toggling application themes.

---
+++ customer_portal/src/composables/useFullscreenSwitch.ts
@@ -0,0 +1,12 @@
+import { useFullscreen } from "@vueuse/core"
+
+const { toggle, isFullscreen } = useFullscreen()
+
+export function useFullscreenSwitch() {
+	return {
+		toggle: () => {
+			toggle()
+		},
+		isFullscreen
+	}
+}
