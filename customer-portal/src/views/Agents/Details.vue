097b68a9e27cb1346f1b16b4f73f52d7bec67e51
Davide Di Modica <webmaster.ddm@gmail.com>
2026-04-15 16:02:05 +0200

    feat(agents): update agent details view to use AgentDetails component and adjust agent ID handling

--- customer-portal/src/views/Agents/Details.vue
+++ customer-portal/src/views/Agents/Details.vue
@@ -1,14 +1,13 @@
 <template>
 	<div class="page @container flex flex-col gap-6">
-		<AlertDetails v-if="alertId" :alert-id />
+		<AgentDetails v-if="agentId" :agent-id />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from "vue-router"
-import AlertDetails from "@/components/alerts/AlertDetails"
-import { toNumber } from "@/utils/format"
+import AgentDetails from "@/components/agents/AgentDetails"
 
 const route = useRoute()
-const alertId = route.params.id ? toNumber(route.params.id as string) : null
+const agentId = route.params.id ? `${route.params.id}` : null
 </script>
