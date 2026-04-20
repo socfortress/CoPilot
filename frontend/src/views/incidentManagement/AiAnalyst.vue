a16329958934be8344291a622f97243c1001d567
Davide Di Modica <webmaster.ddm@gmail.com>
2026-04-20 12:30:41 +0200

    refactor(ai-analyst): simplify tabs layout

--- frontend/src/views/incidentManagement/AiAnalyst.vue
+++ frontend/src/views/incidentManagement/AiAnalyst.vue
@@ -1,22 +1,14 @@
 <template>
 	<div class="page flex flex-col">
 		<n-tabs v-model:value="activeTab" type="line" animated>
-			<n-tab-pane name="overview" display-directive="show:lazy">
-				<template #tab>
-					<div class="flex items-center gap-1.5">
-						<Icon name="carbon:machine-learning-model" :size="14" class="text-primary" />
-						<span>Overview</span>
-					</div>
-				</template>
+			<n-tab-pane name="overview" tab="Overview" display-directive="show:lazy">
 				<TalonOverview />
 			</n-tab-pane>
 			<n-tab-pane name="reports" tab="Reports" display-directive="show:lazy">
 				<AlertsReportsList />
 			</n-tab-pane>
-			<n-tab-pane name="talon" tab="Talon Chat" display-directive="show:lazy" class="p-0!">
-				<div class="h-[calc(100vh-200px)]">
-					<TalonChat />
-				</div>
+			<n-tab-pane name="talon" tab="Talon Chat" display-directive="show:lazy">
+				<TalonChat />
 			</n-tab-pane>
 		</n-tabs>
 	</div>
@@ -28,7 +20,6 @@ import { ref } from "vue"
 import AlertsReportsList from "@/components/aiAnalyst/AlertsReportsList.vue"
 import TalonChat from "@/components/aiAnalyst/TalonChat.vue"
 import TalonOverview from "@/components/aiAnalyst/TalonOverview.vue"
-import Icon from "@/components/common/Icon.vue"
 
 const activeTab = ref("overview")
 </script>
