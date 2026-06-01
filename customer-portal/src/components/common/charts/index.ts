3c4d1fad54ab146fa76c3cc587ab082abc90eccd
Davide Di Modica <webmaster.ddm@gmail.com>
2026-06-01 21:14:22 +0200

    refactor(charts): Migrate to ECharts library

---
+++ customer-portal/src/components/common/charts/index.ts
@@ -0,0 +1,23 @@
+export { CHART_COLORS, CHART_GRID_CONTAIN_AXIS_LABELS } from "./chartConstants"
+export {
+	buildChartTooltipGlassBase,
+	chartTooltipThemeFromStyle,
+	formatChartTooltipAxisFirst,
+	formatChartTooltipAxisMultiSeries,
+	formatChartTooltipAxisMultiSeriesFromParams,
+	formatChartTooltipPieItem,
+	formatChartTooltipTimeAxisFirst,
+	formatChartTooltipWithMarker,
+	getChartTooltipGlassExtraCss,
+	resolveChartItemColor,
+	resolveChartTooltipMarker
+} from "./chartTooltip"
+export type {
+	ChartTooltipAxisFormatterOptions,
+	ChartTooltipAxisMultiSeriesOptions,
+	ChartTooltipAxisMultiSeriesRow,
+	ChartTooltipBorder,
+	ChartTooltipPieFormatterOptions,
+	ChartTooltipTheme,
+	ChartTooltipTimeAxisFormatterOptions
+} from "./chartTooltip"
